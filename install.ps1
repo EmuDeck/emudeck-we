5de3e3908fdabdec33e0601c72add6d3e9b0a8ca
Rodrigo <rodrigosedano@me.com>
2022-12-24 19:36:26 +0100

    func paths

--- install.ps1
+++ install.ps1
@@ -8,36 +8,36 @@ $Host.UI.RawUI.WindowTitle = "EmuDeck Windows Edition Alpha Installer";
 # Functions
 #
 
-Set-Location $env:USERPROFILE\EmuDeck\backend\
-
-. .\functions\showListDialog.ps1
-. .\functions\waitForWinRar.ps1
-. .\functions\download.ps1
-. .\functions\downloadCore.ps1
-. .\functions\showNotification.ps1
-. .\functions\moveFromTo.ps1
-. .\functions\copyFromTo.ps1
-. .\functions\waitForUser.ps1
-. .\functions\sedFile.ps1
-. .\functions\createLink.ps1
-. .\functions\createLauncher.ps1
 
 
-. .\functions\EmuScripts\emuDeckCemu.ps1
-. .\functions\EmuScripts\emuDeckCitra.ps1
-. .\functions\EmuScripts\emuDeckDolphin.ps1
-. .\functions\EmuScripts\emuDeckDuckStation.ps1
-. .\functions\EmuScripts\emuDeckPCSX2.ps1
-. .\functions\EmuScripts\emuDeckRetroArch.ps1
-. .\functions\EmuScripts\emuDeckRPCS3.ps1
-. .\functions\EmuScripts\emuDeckTemplate.ps1
-. .\functions\EmuScripts\emuDeckXemu.ps1
-. .\functions\EmuScripts\emuDeckXenia.ps1
-. .\functions\EmuScripts\emuDeckYuzu.ps1
-. .\functions\EmuScripts\emuDeckRyujinx.ps1
-. .\functions\EmuScripts\emuDeckPPSSPP.ps1
-. .\functions\ToolScripts\emuDeckESDE.ps1
-. .\functions\ToolScripts\emuDeckSRM.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\showListDialog.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\waitForWinRar.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\download.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\downloadCore.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\showNotification.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\moveFromTo.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\copyFromTo.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\waitForUser.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\sedFile.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\createLink.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\createLauncher.ps1
+
+
+. $env:USERPROFILE\EmuDeck\backend\functions\EmuScripts\emuDeckCemu.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\EmuScripts\emuDeckCitra.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\EmuScripts\emuDeckDolphin.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\EmuScripts\emuDeckDuckStation.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\EmuScripts\emuDeckPCSX2.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\EmuScripts\emuDeckRetroArch.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\EmuScripts\emuDeckRPCS3.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\EmuScripts\emuDeckTemplate.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\EmuScripts\emuDeckXemu.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\EmuScripts\emuDeckXenia.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\EmuScripts\emuDeckYuzu.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\EmuScripts\emuDeckRyujinx.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\EmuScripts\emuDeckPPSSPP.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\ToolScripts\emuDeckESDE.ps1
+. $env:USERPROFILE\EmuDeck\backend\functions\ToolScripts\emuDeckSRM.ps1
 
 
 
