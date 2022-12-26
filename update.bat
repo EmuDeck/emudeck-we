4bf8979a88c5b826ff3ec9a7d5c4807937317045
Rodrigo <rodrigosedano@me.com>
2022-12-26 23:07:01 +0100

    dev

--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
-curl -L --output main.zip --url "https://github.com/EmuDeck/emudeck-we/archive/refs/heads/main.zip"
+curl -L --output main.zip --url "https://github.com/EmuDeck/emudeck-we/archive/refs/heads/dev.zip"
 powershell -ExecutionPolicy Bypass -command "& { Expand-Archive -Path main.zip -DestinationPath .\ -Force; Remove-Item main.zip -Recurse -Force -Confirm:$false; robocopy EmuDeck-we-main $env:USERPROFILE\EmuDeck\backend /s /Move /NFL /NDL /NJH /NJS /nc /ns /np; Remove-Item EmuDeck-we-main -Recurse -Force -Confirm:$false }" 
 powershell -ExecutionPolicy Bypass . $env:USERPROFILE\EmuDeck\backend\install.ps1
