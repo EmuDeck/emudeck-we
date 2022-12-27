b5ef33e23a6ef59df04cb1d5b9846971abaedc17
Rodrigo <rodrigosedano@me.com>
2022-12-27 12:50:57 +0100

    test script fixes

--- test.ps1
+++ test.ps1
@@ -48,17 +48,20 @@ Clear-Host
 
 cp "$env:USERPROFILE\EmuDeck\backend\settings.ps1" "$env:USERPROFILE\EmuDeck\settings.ps1"
 
-#
-# UI & Settings creation
-#
-. $env:USERPROFILE\EmuDeck\backend\ui.ps1
 
-#
-# Installation
-#
+$winPath='E:'
+#Set initial Settings
+setSetting 'emulationPath' "$winPath\Emulation\"
+setSetting 'romsPath' "$winPath\Emulation\roms\"
+setSetting 'biosPath' "$winPath\Emulation\bios\"
+setSetting 'toolsPath' "$winPath\Emulation\tools\"
+setSetting 'savesPath' "$winPath\Emulation\saves\"
+setSetting 'storagePath' "$winPath\Emulation\storagePath\"
+setSetting 'ESDEscrapData' "$winPath\Emulation\tools\downloaded_media\"
+Clear-Host
 
+#Load Settings
 . $env:USERPROFILE\EmuDeck\settings.ps1
-
 Set-Location $emulationPath
 
 
