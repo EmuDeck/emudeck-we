4e71e05a6062a2bfd21eba1975dab094bfb7fde3
Rodrigo <rodrigosedano@me.com>
2023-01-22 01:23:05 +0100

    default settings fix

--- settings.ps1
+++ settings.ps1
@@ -45,13 +45,13 @@ $RABezels="true"
 $RAautoSave="false"
 
 #Default installation folders
-$emulationPath=C:/Emulation
-$romsPath=C:/Emulation/roms
-$toolsPath=C:/Emulation/tools
-$biosPath=C:/Emulation/bios
-$savesPath=C:/Emulation/saves
-$storagePath=C:/Emulation/storage
-$ESDEscrapData=C:/Emulation/tools/downloaded_media
+$emulationPath=C:\Emulation
+$romsPath=C:\Emulation\roms
+$toolsPath=C:\Emulation\tools
+$biosPath=C:\Emulation\bios
+$savesPath=C:\Emulation\saves
+$storagePath=C:\Emulation\storage
+$ESDEscrapData=C:\Emulation\tools\downloaded_media
 
 #Default ESDE Theme
 $esdeTheme="EPICNOIR"
