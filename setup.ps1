1d2fb54f81c861f9b44abba2f4bc30e6cea38f08
Rodrigo <rodrigosedano@me.com>
2023-01-27 00:14:07 +0100

    transcript path

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,4 @@
-Start-Transcript $env:USERPROFILE\emudeck\emudeck.log
+Start-Transcript $env:USERPROFILE/emudeck/emudeck.log
 
 #We install 7zip
 winget install -e --id 7zip.7zip --accept-package-agreements --accept-source-agreements
