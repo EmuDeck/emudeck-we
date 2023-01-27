041b3310583e2aa2d1f00d66a37470d0329e5394
Rodrigo <rodrigosedano@me.com>
2023-01-27 15:45:35 +0100

    launcher fix

--- functions/createLauncher.ps1
+++ functions/createLauncher.ps1
@@ -2,7 +2,5 @@
 function createLauncher($name, $exe){
   $SourceFilePath = -join($emulationPath,'\tools\EmulationStation-DE\Emulators\',$name,'\',$exe,'.exe')
   $ShortcutPath = -join($emulationPath,'\tools\launchers\',$exe,'.lnk')
-  mkdir 'saves\duckstation' -ErrorAction SilentlyContinue
-  mkdir $SourceFilePath -ErrorAction SilentlyContinue
   createLink $SourceFilePath $ShortcutPath
 }
