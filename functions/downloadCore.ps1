4c97d591137cb23ae4ffc8b94518fff47a4c2a8c
Rodrigo <rodrigosedano@me.com>
2023-02-17 00:39:05 +0100

    -y

--- functions/downloadCore.ps1
+++ functions/downloadCore.ps1
@@ -7,7 +7,7 @@ function downloadCore($url, $core) {
 	foreach ($line in $destination) {
 		$extn = [IO.Path]::GetExtension($line)
 		if ($extn -eq ".zip" ){						
-			7z x -yo"tools\EmulationStation-DE\Emulators\RetroArch\cores\" $destination		
+			7z x -o"tools\EmulationStation-DE\Emulators\RetroArch\cores\" $destination		
 			Start-Sleep -Seconds 0.5
 			Remove-Item $destination
 		}
