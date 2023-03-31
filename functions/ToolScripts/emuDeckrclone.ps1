e7dc455a96f1f88fd4c29eff5b2d0a7fe2cc5011
Rodrigo <rodrigosedano@me.com>
2023-03-31 19:37:18 +0200

    rclone

---
+++ functions/ToolScripts/emuDeckrclone.ps1
@@ -0,0 +1,42 @@
+$rclone_path="$toolsPath/rclone"
+$rclone_bin="$rclone_path/rclone"
+$rclone_config="$rclone_path/rclone.conf"
+$rclone_releaseURL = getLatestReleaseURLGH 'rclone/rclone' 'zip' 'windows-amd64'
+
+function rclone_install($rclone_provider){	
+		
+	download $rclone_releaseURL "rclone.zip"	
+	
+	$regex = '^.*\/(rclone-v\d+\.\d+\.\d+-windows-amd64\.zip)$'
+	
+	if ($rclone_releaseURL -match $regex) {
+		
+				$filename = $matches[1]
+		
+		$filename = $filename.Replace('.zip','')
+		
+		Rename-Item "temp\rclone\$filename" -NewName "rclone" 
+		
+		moveFromTo "temp/rclone/" "$toolsPath\"	
+		Copy-Item "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\configs\rclone\rclone.conf" -Destination "$toolsPath/rclone"
+		rm -fo  "temp\rclone" -Recurse
+	}
+
+	& $rclone_bin config update "$rclone_provider" 
+	
+	setSetting "rclone_provider" "$rclone_provider"
+	. $env:USERPROFILE\AppData\Roaming\EmuDeck\backend\functions\all.ps1
+	
+}
+
+function rclone_downloadEmu($emuName, $path){	
+	if (Test-Path "$rclone_bin") {
+		& $rclone_bin sync -P -L "$path" "$rclone_provider`:Emudeck\saves\$emuName\"
+	}
+}
+
+function rclone_uploadEmu($emuName, $path){	
+  if (Test-Path "$rclone_bin") {
+	  & $rclone_bin sync -P -L "$rclone_provider`:Emudeck\saves\$emuName\" "$path" 
+  }
+}
