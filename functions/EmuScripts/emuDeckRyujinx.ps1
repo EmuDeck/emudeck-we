a3c55361670fff8620c3d3a409310418bfcd375a
Rodrigo <rodrigosedano@me.com>
2023-05-04 16:05:30 +0200

    fix

--- functions/EmuScripts/emuDeckRyujinx.ps1
+++ functions/EmuScripts/emuDeckRyujinx.ps1
@@ -74,7 +74,10 @@ function Ryujinx_finalize(){
 	echo "NYI"
 }
 function Ryujinx_IsInstalled(){
-	echo "NYI"
+	$test=Test-Path -Path "tools\EmulationStation-DE\Emulators\Ryujinx"
+	if($test){
+		echo "true"
+	}
 }
 function Ryujinx_resetConfig(){
 	echo "NYI"
