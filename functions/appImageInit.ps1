093bbf481c3763c167f2ece8947a973266440822
Rodrigo <rodrigosedano@me.com>
2023-05-24 19:08:00 +0200

    fix appimageinit

--- functions/appImageInit.ps1
+++ functions/appImageInit.ps1
@@ -1,5 +1,5 @@
 function appImageInit(){
-	if ($cloud_sync_status -eq $null) {
+	if ((Test-Path "$cloud_sync_bin") -and ($cloud_sync_status -eq $null)) {	
 		setSetting "cloud_sync_status" "true"
 	}
 }
