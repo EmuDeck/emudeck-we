afbb1cf8f5a248b8df74a96b54b3572d2fbedb1c
Rodrigo <rodrigosedano@me.com>
2023-05-24 18:18:29 +0200

    Fixes setSetting function

--- functions/helperFunctions.ps1
+++ functions/helperFunctions.ps1
@@ -1,22 +1,22 @@
- function setSetting($old, $new){
-
+function setSetting($old, $new) {
 	$fileToCheck = "$userFolder\EmuDeck\settings.ps1"
 
 	$fileContents = Get-Content $fileToCheck
 	$line = $fileContents | Select-String $old | Select-Object -ExpandProperty Line
-	if ($line){
-		$newLine=-join('$',$old,'=','"',$new,'"')
-		$modifiedContents = $fileContents | ForEach-Object {$_.Replace($line,$newLine)}
-	
-		Set-Content -Path $fileToCheck -Value $modifiedContents
-	
-		echo "Line $line changed to $newLine"
-	}else{
-		$newLine=-join('$',$old,'=','"',$new,'"')
-		Add-Content $fileToCheck $newLine
-		echo "Line created on $fileToCheck"
-	}
+	if ($line) {
+		$newLine = -join('$', $old, '=', '"', $new, '"')
+		$modifiedContents = $fileContents | ForEach-Object { $_.Replace($line, $newLine) }
 
+		Set-Content -Path $fileToCheck -Value $modifiedContents
+
+		Write-Host "Line '$line' changed to '$newLine'"
+	} else {
+		$newLine = -join('$', $old, '=', '"', $new, '"')
+		$newLine += "`r`n"  # Agregar nueva línea al final del contenido
+		Add-Content $fileToCheck $newLine
+
+		Write-Host "New line '$newLine' created in $fileToCheck"
+	}
 }
 
  function setConfig($old, $new, $fileToCheck){	
