f0bd074619bd96fa0c61d52934c4eb834d96dbd4
Rodrigo <rodrigosedano@me.com>
2023-05-25 11:32:40 +0200

    remove zips

--- functions/download.ps1
+++ functions/download.ps1
@@ -1,7 +1,7 @@
 function download($url, $file) {
 	$wc = New-Object net.webclient		
 	$destination="$temp/$file"		
-	mkdir $temp -ErrorAction SilentlyContinue	
+	mkdir $temp -ErrorAction SilentlyContinue
 	
 	$wc.Downloadfile($url, $destination)
 	
@@ -10,7 +10,7 @@ function download($url, $file) {
 		if ($extn -eq ".zip" ){			
 			$dir = $file.replace('.zip','')
 			7z x -o"$temp/$dir" -aoa "$temp/$file"
-			#Remove-Item $temp/$file
+			Remove-Item $temp/$file
 		}
 		if ($extn -eq ".7z" ){
 			$dir = $output.replace('.7z','')
