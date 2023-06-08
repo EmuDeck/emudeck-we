9d0144e017182ca05e53161850471e9c0190b3d8
Rodrigo <rodrigosedano@me.com>
2023-06-08 23:02:56 +0200

    back to steam

--- tools/gamemode/desktopmode.bat
+++ tools/gamemode/desktopmode.bat
@@ -81,3 +81,8 @@ echo|set /p="Escape - Restarting sihost.exe ................................... 
 start sihost.exe > NUL 2>NUL
 
 echo OK!
+
+
+echo|set /p="Escape - Changing shell to explorer.exe .......................... "
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "%USERPROFILE%\AppData\Roaming\EmuDeck\backend\tools\gamemode\logon.bat" /f > NUL 2>NUL
+IF %ERRORLEVEL% == 0 ( ECHO OK! ) ELSE ( ECHO FAIL! )
