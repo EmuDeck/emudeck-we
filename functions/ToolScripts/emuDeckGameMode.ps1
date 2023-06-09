d28ff491579da72ea017447e0f59cc12dcfd1a97
Rodrigo <rodrigosedano@me.com>
2023-06-09 22:33:16 +0200

    userFolder

--- functions/ToolScripts/emuDeckGameMode.ps1
+++ functions/ToolScripts/emuDeckGameMode.ps1
@@ -1,12 +1,12 @@
 function game_mode_enable(){
-	& %USERPROFILE%\AppData\Roaming\EmuDeck\backend\tools\gamemode\enablegamemode.bat
+	& $userFolder\AppData\Roaming\EmuDeck\backend\tools\gamemode\enablegamemode.bat
 	if($?){
 		echo "true"
 	}
 }
 
 function game_mode_disable(){
-	& %USERPROFILE%\AppData\Roaming\EmuDeck\backend\tools\gamemode\disablegamemode.bat
+	& $userFolder\AppData\Roaming\EmuDeck\backend\tools\gamemode\disablegamemode.bat
 	if($?){
 		echo "true"
 	}
