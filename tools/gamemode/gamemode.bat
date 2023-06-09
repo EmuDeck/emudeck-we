69a7554ae377597252ee947062c2a6dca98480f6
Rodrigo <rodrigosedano@me.com>
2023-06-10 00:25:20 +0200

    details

--- tools/gamemode/gamemode.bat
+++ tools/gamemode/gamemode.bat
@@ -63,14 +63,12 @@ echo|set /p="Setting up Steam UI..."
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "cmd /c start /min """""" ""%USERPROFILE%\AppData\Roaming\EmuDeck\backend\tools\gamemode\logon.bat""" /f > NUL 2>NUL
 IF %ERRORLEVEL% == 0 ( ECHO OK! ) ELSE ( ECHO FAIL! )
 taskkill /F /IM sihost.exe > NUL 2>NUL
-echo OK!
-
 
 echo|set /p="Waiting for changes to be enabled, this will take some seconds..." 
 timeout /T 5 /nobreak > NUL 2>NUL
 start C:\Windows\System32\sihost.exe > NUL 2>NUL
 timeout /T 5 /nobreak > NUL 2>NUL
-echo OK!
+IF %ERRORLEVEL% == 0 ( ECHO OK! ) ELSE ( ECHO FAIL! )
 
 taskkill /f /im explorer.exe
 "C:\Program Files (x86)\Steam\steam.exe" "-bigpicture" && cmd /c start /min "" "%USERPROFILE%\AppData\Roaming\EmuDeck\backend\tools\gamemode\desktopmode.bat"
