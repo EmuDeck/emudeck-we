d209c71f2e26bf299782c3b9be9dffa93ec4458d
Rodrigo <rodrigosedano@me.com>
2023-06-10 00:11:33 +0200

    """"""

--- tools/gamemode/enablegamemode.bat
+++ tools/gamemode/enablegamemode.bat
@@ -60,7 +60,7 @@
 
 
 echo|set /p="Setting up Steam UI..."
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "cmd /c start /min """" ""%USERPROFILE%\AppData\Roaming\EmuDeck\backend\tools\gamemode\logon.bat""" /f > NUL 2>NUL
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "cmd /c start /min """""" ""%USERPROFILE%\AppData\Roaming\EmuDeck\backend\tools\gamemode\logon.bat""" /f > NUL 2>NUL
 IF %ERRORLEVEL% == 0 ( ECHO OK! ) ELSE ( ECHO FAIL! )
 taskkill /F /IM sihost.exe > NUL 2>NUL
 echo|set /p="Waiting for changes to be enabled, this will take some seconds..." 
@@ -70,6 +70,6 @@ IF %ERRORLEVEL% == 0 ( ECHO OK! ) ELSE ( ECHO FAIL! )
 timeout /T 5 /nobreak > NUL 2>NUL
 
 ::taskkill /f /im explorer.exe
-::"C:\Program Files (x86)\Steam\steam.exe" "-bigpicture"  && ::"%USERPROFILE%\AppData\Roaming\EmuDeck\backend\tools\gamemode\desktopmode.bat"
+::"C:\Program Files (x86)\Steam\steam.exe" "-bigpicture"  && "%USERPROFILE%\AppData\Roaming\EmuDeck\backend\tools\gamemode\desktopmode.bat"
 ::
 ::echo|set /p="Starting Steam UI" 
