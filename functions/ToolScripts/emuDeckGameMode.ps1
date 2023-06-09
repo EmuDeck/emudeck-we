4d824e0b96a6bb1c0440ff2d026bfdd2ba203915
Rodrigo <rodrigosedano@me.com>
2023-06-09 23:59:01 +0200

    icon

--- functions/ToolScripts/emuDeckGameMode.ps1
+++ functions/ToolScripts/emuDeckGameMode.ps1
@@ -1,7 +1,14 @@
 function game_mode_enable(){
-	$source = "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\gamemode\gamemode.bat"
-	$target =  "$env:USERPROFILE\Desktop\Return to Gaming Mode.lnk"
-	createLink($source, $target)	
+	$source = "$userFolder\AppData\Roaming\EmuDeck\backend\tools\gamemode\gamemode.bat"
+	$target =  "$userFolder\Desktop\Return to Gaming Mode.lnk"
+	
+	$WScriptObj = New-Object -ComObject ("WScript.Shell")
+	$shortcut = $WscriptObj.CreateShortcut($target)
+	$shortcut.TargetPath = $source
+	$shortcut.WindowStyle = 1	
+	$shortcut.IconLocation = "$userFolder\AppData\Roaming\EmuDeck\backend\tools\gamemode\icon.ico"
+	$shortcut.Save()
+
 	& $userFolder\AppData\Roaming\EmuDeck\backend\tools\gamemode\enablegamemode.bat
 	if($?){
 		echo "true"
