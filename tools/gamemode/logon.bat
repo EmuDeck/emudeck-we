0426b0a63609b25f56e8085c4d6d8db37effe1a6
Rodrigo <rodrigosedano@me.com>
2023-06-10 00:28:04 +0200

    no bg change

--- tools/gamemode/logon.bat
+++ tools/gamemode/logon.bat
@@ -1,5 +1,4 @@
 @echo off 
-REG ADD "HKCU\Control Panel\Desktop" /v Wallpaper /d "%USERPROFILE%\AppData\Roaming\EmuDeck\backend\tools\gamemode\bg.png" /f
 echo|set /p="Setting up Steam UI..."
  "C:\Program Files (x86)\Steam\Steam.exe" "-bigpicture" && cmd /c start /min ""  "%USERPROFILE%\AppData\Roaming\EmuDeck\backend\tools\gamemode\desktopmode.bat"
  exit
