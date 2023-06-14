5bac8dd94b28fc11640ff256a64df38d0e72ca5a
Rodrigo <rodrigosedano@me.com>
2023-06-14 18:12:13 +0200

    WII

--- functions/EmuScripts/emuDeckDolphin.ps1
+++ functions/EmuScripts/emuDeckDolphin.ps1
@@ -46,8 +46,9 @@ function Dolphin_setupSaves(){
 	createLink $SourceFilePath $ShortcutPath
 	
 	#Saves Wii
-	$SourceFilePath = "$emusPath\Dolphin-x64\User\Wii"
-	$ShortcutPath = -join($emulationPath,"\saves\dolphin\Wii.lnk")
+	$SourceFilePath = "$emusPath\Dolphin-x64\User\WII"
+	$ShortcutPath = -join($emulationPath,"\saves\dolphin\WII.lnk")
+	rm -fo  $ShortcutPath -Recurse -ErrorAction SilentlyContinue
 	mkdir "saves\dolphin" -ErrorAction SilentlyContinue
 	mkdir $SourceFilePath -ErrorAction SilentlyContinue
 	createLink $SourceFilePath $ShortcutPath
