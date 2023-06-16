fd7f9a011b6e54c0ee160689ac0b186cc185c4e4
Rodrigo <rodrigosedano@me.com>
2023-06-16 17:51:18 +0200

    version bump + latest RA

--- vars.ps1
+++ vars.ps1
@@ -1,8 +1,8 @@
 $EmuDeckStartFolder = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\EmuDeck"
-$url_ra = "https://buildbot.libretro.com/stable/1.15.0/windows/x86_64/RetroArch.7z"
+$url_ra = "https://buildbot.libretro.com/nightly/windows/x86_64/RetroArch.7z"
 $url_dolphin = "https://dl.dolphin-emu.org/builds/40/56/dolphin-master-5.0-18498-x64.7z"
 $url_cemu = "https://cemu.info/releases/cemu_1.27.1.zip"
-$url_ppsspp = "https://ppsspp.org/files/1_14_4/ppsspp_win.zip"
+$url_ppsspp = "https://ppsspp.org/files/1_15_4/ppsspp_win.zip"
 $url_pcsx2 = "https://github.com/dragoonDorise/rpcsx2win/releases/download/1.7.3930/pcsx2.7z"
 $url_esde = "https://gitlab.com/es-de/emulationstation-de/-/package_files/76389046/download"
 $userFolder = $env:USERPROFILE
