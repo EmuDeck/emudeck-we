7a3d1eeae45ecbc901382842072dacfdd92d3f06
Rodrigo <rodrigosedano@me.com>
2023-06-19 22:56:16 +0200

    fixes possible bad installations on C

--- functions/appImageInit.ps1
+++ functions/appImageInit.ps1
@@ -1,20 +1,5 @@
 function appImageInit(){
-	if ((Test-Path "$cloud_sync_bin") -and ($cloud_sync_status -eq $null)) {	
-		setSetting "cloud_sync_status" "true"
-	}
-	if ($melondsResolution -eq $null) {	
-		setSetting "melondsResolution" "720P"
-	}
-	Cemu_setupSaves
-	Citra_setupSaves
-	Dolphin_setupSaves
-	RPCS3_setupSaves
-	Yuzu_setupSaves
-	PCSX2_setupSaves
-	PCSX2QT_setupSaves
-	melonDS_setupSaves
-	RetroArch_setupSaves
-	Ryujinx_setupSaves
-	Duckstation_setupSaves
-	createLauncher "cemu"
+
+	sedFile $userFolder/EmuDeck/settings.ps1 """:\Emulation" """C:\Emulation"
+
 }
