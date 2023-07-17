d553e3eae0aa883036b603af72353f4501a45a02
Rodrigo <rodrigosedano@me.com>
2023-07-17 15:38:39 +0200

    update ESDE launcher

--- tools/launchers/esde/EmulationStationDE.bat
+++ tools/launchers/esde/EmulationStationDE.bat
@@ -1,8 +1,19 @@
 @Write-Output off
 set args=%*
 
-powershell -ExecutionPolicy Bypass -command "& { . $env:USERPROFILE/AppData/Roaming/EmuDeck/backend/functions/all.ps1 ; cloud_sync_downloadEmuAll "}
+for /f "tokens=2 delims==" %%a in ('type "%userprofile%\EmuDeck\settings.ps1" ^| find "$toolsPath"') do set "toolsPath=%%~a"
+for /f "tokens=2 delims==" %%b in ('type "%userprofile%\EmuDeck\settings.ps1" ^| find "cloud_sync_status"') do set "cloud_sync_status=%%~b"
+set rcloneConfig="%toolsPath%\rclone\rclone.conf"
+if exist "%rcloneConfig%" (
+	if "%cloud_sync_status%"=="true" (
+		powershell -ExecutionPolicy Bypass -command "& { . $env:USERPROFILE/AppData/Roaming/EmuDeck/backend/functions/all.ps1 ; cloud_sync_downloadEmuAll "}		
+	)
+)
+$env:USERPROFILE/AppData/Roaming/EmuDeck/backend/functions/all.ps1 ; cloud_sync_downloadEmuAll "}
 "ESDEPATH\EmulationStation.exe" %args%
 cls
-
-powershell -ExecutionPolicy Bypass -command "& { . $env:USERPROFILE/AppData/Roaming/EmuDeck/backend/functions/all.ps1 ; cloud_sync_uploadEmuAll "}
+if exist "%rcloneConfig%" (
+	if "%cloud_sync_status%"=="true" (
+		powershell -ExecutionPolicy Bypass -command "& { . $env:USERPROFILE/AppData/Roaming/EmuDeck/backend/functions/all.ps1 ; cloud_sync_uploadEmuAll "}		
+	)
+)
