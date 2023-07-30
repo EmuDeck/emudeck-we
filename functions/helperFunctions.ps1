e5cfc9c50ce01cb55a33ef7b68039a576bd0dc0c
Rodrigo <rodrigosedano@me.com>
2023-07-30 19:51:33 +0200

    moar fixes

--- functions/helperFunctions.ps1
+++ functions/helperFunctions.ps1
@@ -419,10 +419,11 @@ function createSaveLink($simLinkPath, $emuSavePath){
 			# We copy the saves to the Emulation/saves Folder adn create a backup
 			echo "Migrating saves"
 			#Move-Item -Path "$simLinkPath\*" -Destination $emuSavePath -Force						
-			Copy-Item -Path "$simLinkPath" -Destination $emuSavePath\ -Recurse			
+			Copy-Item -Path "$simLinkPath\*" -Destination $emuSavePath -Recurse
+			
 			if ($?) {
-				echo "rename"
-				Rename-Item -Path "$simLinkPath" -NewName "$simLinkPath"_bak
+				$backupName = "$simLinkPath" + "_bak"
+				Rename-Item -Path "$simLinkPath" -NewName "$backupName"
 			}
 		}	
 	}	
