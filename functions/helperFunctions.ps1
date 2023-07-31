888051a8b1a5cc07af6c06bc630fab01ea7a3489
Rodrigo <rodrigosedano@me.com>
2023-07-31 14:28:37 +0200

    toast notif

--- functions/helperFunctions.ps1
+++ functions/helperFunctions.ps1
@@ -562,3 +562,39 @@ function createSaveLink($simLinkPath, $emuSavePath){
 	
 }
 
+
+function toastNotification {
+	param(
+		[string]$Title,
+		[string]$Message,
+		[string]$img
+	)
+
+	#Specify Launcher App ID
+	$LauncherID = "{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe"
+
+	#Load Assemblies
+	[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
+	[Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime] | Out-Null
+
+	#Build XML Template
+	$ToastTemplate = @"
+<toast duration="long">
+	<visual>
+		<binding template="ToastImageAndText03">
+			<text id="1">$Title</text>
+			<text id="2">$Message</text>
+			<image id="1" src="$img" />
+		</binding>
+	</visual>
+</toast>
+"@
+
+	#Prepare XML
+	$ToastXml = [Windows.Data.Xml.Dom.XmlDocument]::New()
+	$ToastXml.LoadXml($ToastTemplate)
+
+	#Prepare and Create Toast
+	$ToastMessage = [Windows.UI.Notifications.ToastNotification]::New($ToastXML)
+	[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($LauncherID).Show($ToastMessage)
+}
