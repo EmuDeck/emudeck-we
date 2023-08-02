c39c2c87a24bdd7eb70e4b1b2e2fdd2ed18e816c
Rodrigo <rodrigosedano@me.com>
2023-08-03 00:09:25 +0200

    testing

--- functions/ToolScripts/emuDeckSaveSync.ps1
+++ functions/ToolScripts/emuDeckSaveSync.ps1
@@ -365,7 +365,6 @@ function cloud_sync_download($emuName){
 					if (Test-Path -PathType Container $folder) {
 						Set-Content -Path "$lastUploadFile" -Value $timestamp
 						Remove-Item -Path "$failUploadFile" -Force -Recurse -ErrorAction SilentlyContinue
-						cscript //nologo "$env:USERPROFILE/AppData/Roaming/EmuDeck/backend/tools/cloud_sync_monitor.vbs" "all"
 					}
 				}								
 			}
@@ -373,9 +372,6 @@ function cloud_sync_download($emuName){
 			$dialog = cleanDialog -TitleText "CloudSync" -MessageText "Downloading saves for $emuName, please wait..."			
 			$target = "$emulationPath\saves\$emuName\"
 			& $cloud_sync_bin copy --fast-list --checkers=50 --exclude=/.fail_upload --exclude=/.fail_download --exclude=/.pending_upload "$target" "$cloud_sync_provider`:Emudeck\saves\$emuName\"
-			if ($?) {	
-				cscript //nologo "$env:USERPROFILE/AppData/Roaming/EmuDeck/backend/tools/cloud_sync_monitor.vbs" "$emuName"
-			}	
 		}	
 
 		$dialog.Close()
