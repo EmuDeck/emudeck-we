56a0b89249ffd686f5661f5075bf936433d679fa
Rodrigo <rodrigosedano@me.com>
2023-08-18 13:27:11 +0200

    yuzu setupsaves

--- functions/EmuScripts/emuDeckYuzu.ps1
+++ functions/EmuScripts/emuDeckYuzu.ps1
@@ -22,17 +22,6 @@ function Yuzu_init(){
 	Copy-Item "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\configs\yuzu\SDL2.dll" -Destination "$emusPath\yuzu\yuzu-windows-msvc\" -ErrorAction SilentlyContinue
 	
 	sedFile $destination\qt-config.ini "C:\Emulation" $emulationPath
-
-	setMSG "Yuzu - Creating Keys & Firmware Links"
-	#Firmware
-	$simLinkPath = "$emusPath\yuzu\yuzu-windows-msvc\user\nand\system\Contents\registered"
-	$emuSavePath = -join($emulationPath,"\bios\yuzu\firmware")
-	createSaveLink $simLinkPath $emuSavePath
-	
-	#Keys
-	$simLinkPath = "$emusPath\yuzu\yuzu-windows-msvc\user\keys"
-	$emuSavePath = -join($emulationPath,"\bios\yuzu\keys")
-	createSaveLink $simLinkPath $emuSavePath
 	
 	Yuzu_setupStorage
 #	Yuzu_setupSaves
@@ -47,6 +36,18 @@ function Yuzu_setEmulationFolder(){
 	Write-Output "NYI"
 }
 function Yuzu_setupSaves(){
+
+	setMSG "Yuzu - Creating Keys & Firmware Links"
+	#Firmware
+	$simLinkPath = "$emusPath\yuzu\yuzu-windows-msvc\user\nand\system\Contents\registered"
+	$emuSavePath = -join($emulationPath,"\bios\yuzu\firmware")
+	createSaveLink $simLinkPath $emuSavePath
+	
+	#Keys
+	$simLinkPath = "$emusPath\yuzu\yuzu-windows-msvc\user\keys"
+	$emuSavePath = -join($emulationPath,"\bios\yuzu\keys")
+	createSaveLink $simLinkPath $emuSavePath
+
 	setMSG "Yuzu - Saves Links"
 	$simLinkPath = "$emusPath\yuzu\yuzu-windows-msvc\user\nand\user\save"	
 	$emuSavePath = -join($emulationPath,"\saves\yuzu\saves")
