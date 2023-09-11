514aa9feed47fffaa21decd4aafff927f5545af9
Rodrigo <rodrigosedano@me.com>
2023-09-11 16:16:25 +0200

    new createLauncher for the ps1 files

--- functions/createLauncher.ps1
+++ functions/createLauncher.ps1
@@ -1,33 +1,38 @@
-function createLauncher($exe){
-  $SourceFilePath = "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\tools\launchers\$exe.bat"
-  $ShortcutPath = "$toolsPath\launchers\$exe.bat"
-  Copy-Item -Path $SourceFilePath -Destination $ShortcutPath 
-  
-  sedFile $ShortcutPath "ESDEPATH" "$userFolder\EmuDeck\EmulationStation-DE"
- 
-  #Windows Start Menu
+function createLauncher($ps1) {
+  $SourceFilePath = "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\tools\launchers\$ps1.ps1"
+  $ShortcutPathPs1 = "$toolsPath\launchers\$ps1.ps1"
+
+  # Copiar el archivo .ps1 al directorio de lanzadores
+  Copy-Item -Path $SourceFilePath -Destination $ShortcutPathPs1
+
+  # Modificar el archivo .ps1 si es necesario
+  # Aquí puedes agregar cualquier código de modificación que sea necesario para el archivo .ps1
+
+  # Crear el acceso directo en el menú de inicio
   $cultureInfo = [System.Globalization.CultureInfo]::CurrentCulture
   $textInfo = $cultureInfo.TextInfo
-  $name = $textInfo.ToTitleCase($exe)  
-  
-  if ($name -eq "Esde/EmulationStationDE"){
+  $name = $textInfo.ToTitleCase($ps1)
+
+  if ($name -eq "Esde/EmulationStationDE") {
     $name = "EmulationStationDE"
-   }  
-  
-  mkdir "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\EmuDeck\" -ErrorAction SilentlyContinue
-  $ShortcutPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\EmuDeck\$name.lnk"
-  $TargetPath = "$toolsPath\launchers\$exe.bat" 
-  $WScriptShell = New-Object -ComObject WScript.Shell
-  $Shortcut = $WScriptShell.CreateShortcut($ShortcutPath)
-  if ($name -eq "EmulationStationDE"){
-     $Shortcut.IconLocation = "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\tools\launchers\icons\$name.ico"
-  }else{
-    $Shortcut.IconLocation = "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\tools\launchers\icons\$exe.ico"
   }
 
+  mkdir "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\EmuDeck\" -ErrorAction SilentlyContinue
+  $ShortcutPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\EmuDeck\$name.lnk"
+  $TargetPath = "powershell.exe"  # Ruta al ejecutable de PowerShell
+  $Arguments = "-ExecutionPolicy Bypass -File $ShortcutPathPs1"  # Argumentos para ejecutar el archivo .ps1
+  $WScriptShell = New-Object -ComObject WScript.Shell
+  $Shortcut = $WScriptShell.CreateShortcut($ShortcutPath)
   $Shortcut.TargetPath = $TargetPath
-  $Shortcut.WindowStyle = 7 
+  $Shortcut.Arguments = $Arguments
+  $Shortcut.WindowStyle = 7
+
+  # Configurar el icono (ajusta la ruta al icono según tus necesidades)
+  if ($name -eq "EmulationStationDE") {
+    $Shortcut.IconLocation = "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\tools\launchers\icons\$name.ico"
+  } else {
+    $Shortcut.IconLocation = "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\tools\launchers\icons\$ps1.ico"
+  }
+
   $Shortcut.Save()
-  sedFile $ShortcutPath "ESDEPATH" "$userFolder\EmuDeck\EmulationStation-DE"
- 
 }
