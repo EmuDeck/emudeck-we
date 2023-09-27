f4434ae45fc98a11818f801255c61b6417912271
Rodrigo <rodrigosedano@me.com>
2023-09-27 15:53:39 +0200

    Windows 10 FIPS

--- install.ps1
+++ install.ps1
@@ -7,23 +7,47 @@ function getLatestReleaseURLGH($Repository, $FileType, $FindToMatch, $IgnoreText
 
 	$url = "https://api.github.com/repos/$Repository/releases/latest"
 
-	$url = Invoke-RestMethod -Uri $url | Select-Object -ExpandProperty assets | 
-		   Where-Object { $_.browser_download_url -Match $FindToMatch -and $_.browser_download_url -like "*.$FileType" -and $_.browser_download_url -notlike "*$IgnoreText*" } | 
+	$url = Invoke-RestMethod -Uri $url | Select-Object -ExpandProperty assets |
+		   Where-Object { $_.browser_download_url -Match $FindToMatch -and $_.browser_download_url -like "*.$FileType" -and $_.browser_download_url -notlike "*$IgnoreText*" } |
 		   Select-Object -ExpandProperty browser_download_url | Select-Object -First 1
 		   return $url
 
 	return $url
 }
 
+function startScriptWithAdmin {
+	param (
+		[string]$ScriptContent
+	)
+
+	#$scriptContent = @"
+	#. $env:USERPROFILE\AppData\Roaming\EmuDeck\backend\functions\all.ps1;
+	#Write-Host "I'm Admin"
+	#"@
+
+	#StartScriptWithAdmin -ScriptContent $scriptContent
+
+	$tempScriptPath = [System.IO.Path]::GetTempFileName() + ".ps1"
+	$ScriptContent | Out-File -FilePath $tempScriptPath -Encoding utf8 -Force
+
+	$psi = New-Object System.Diagnostics.ProcessStartInfo
+	$psi.Verb = "runas"
+	$psi.FileName = "powershell.exe"
+	$psi.Arguments = "-NoProfile -ExecutionPolicy Bypass -File ""$tempScriptPath"""
+	[System.Diagnostics.Process]::Start($psi).WaitForExit()
+
+	Remove-Item $tempScriptPath -Force
+}
+
 function download($url, $file) {
 
-	$wc = New-Object net.webclient		
+	$wc = New-Object net.webclient
 	$temp = Join-Path $env:USERPROFILE "Downloads"
 	$destination="$temp/$file"
 	mkdir $temp -ErrorAction SilentlyContinue
-	
+
 	$wc.Downloadfile($url, $destination)
-	
+
 	Write-Host "Done!" -NoNewline -ForegroundColor green -BackgroundColor black
 }
 
@@ -33,25 +57,36 @@ if ( $PSversion -lt 51 ){
 	Write-Host ""
 	Write-Host " Downloading .NET..."
 	download "https://go.microsoft.com/fwlink/?linkid=2088631" "dotNet.exe"
-	$temp = Join-Path $env:USERPROFILE "Downloads" 			
+	$temp = Join-Path $env:USERPROFILE "Downloads"
 	&"$temp/dotNet.exe"
 	rm -fo "$temp/dotNet.exe"
-	
-	Write-Host ""	
+
+	Write-Host ""
 	Write-Host " Downloading WMF 5.1..."
 	download "https://go.microsoft.com/fwlink/?linkid=839516" "wmf51.msu"
-	
-	$temp = Join-Path $env:USERPROFILE "Downloads" 		
-	&"$temp/wmf51.msu"	
+
+	$temp = Join-Path $env:USERPROFILE "Downloads"
+	&"$temp/wmf51.msu"
 	rm -fo "$temp/wmf51.msu"
-	
-	Write-Host ""	
+
+	Write-Host ""
 	Write-Host " If the WMF installation fails please restart Windows and run the installer again"  -ForegroundColor white
-	Read-Host -Prompt "Press any key to continue or CTRL+C to quit" 	
+	Read-Host -Prompt "Press any key to continue or CTRL+C to quit"
 	clear
-	Write-Host "PowerShell updated to 5.1" -ForegroundColor white	
+	Write-Host "PowerShell updated to 5.1" -ForegroundColor white
 }
 
+	$FIPSAlgorithmPolicy = Get-ItemProperty -Path HKLM:\System\CurrentControlSet\Control\Lsa\FIPSAlgorithmPolicy
+	$EnabledValue = $FIPSAlgorithmPolicy.Enabled
+
+	if($EnabledValue -eq 1){
+		Write-Host "Windows 10 FIPS detected, we need to turn it off so cloudSync can be used, after that the computer will restart. Once back in the desktop just run this installer again. You can read about FIPS here and why is better to disable it: https://techcommunity.microsoft.com/t5/microsoft-security-baselines/why-we-re-not-recommending-fips-mode-anymore/ba-p/701037" -ForegroundColor white
+		Read-Host -Prompt "Press any key to apply the fix and restart"
+$scriptContent = @"
+New-ItemProperty -Path HKLM:\System\CurrentControlSet\Control\Lsa\FIPSAlgorithmPolicy -name Enabled -value 0; Restart-Computer -Force
+"@
+			startScriptWithAdmin -ScriptContent $scriptContent
+	}
 
 clear
 
@@ -60,8 +95,8 @@ Write-Host ""
 &winget install -e --id Git.Git --accept-package-agreements --accept-source-agreements
 &winget install -e --id 7zip.7zip --accept-package-agreements --accept-source-agreements
 
-	
-	if (-not (Test-Path "$env:ProgramFiles\7-Zip\7z.exe") -or -not (Test-Path "$env:ProgramFiles\Git\bin\git.exe")) {	
+
+	if (-not (Test-Path "$env:ProgramFiles\7-Zip\7z.exe") -or -not (Test-Path "$env:ProgramFiles\Git\bin\git.exe")) {
 		clear
 		$Host.UI.RawUI.BackgroundColor = "Red"
 		#Clear-Host
@@ -70,14 +105,14 @@ Write-Host ""
 		Write-Host  "https://emudeck.github.io/common-issues/windows/#dependencies" -ForegroundColor white
 		Write-Host "EmuDeck can't be installed."
 		$Host.UI.RawUI.BackgroundColor = "Black"
-		Read-Host -Prompt "Press any key to exit" 	
-	}else{	
+		Read-Host -Prompt "Press any key to exit"
+	}else{
 		Write-Host ""
 		Write-Host "Downloading EmuDeck..." -ForegroundColor white
 		Write-Host ""
 		$url_emudeck = getLatestReleaseURLGH 'EmuDeck/emudeck-electron-early' 'exe' 'emudeck'
 		download $url_emudeck "emudeck_install.exe"
-		$temp = Join-Path $env:USERPROFILE "Downloads" 		
+		$temp = Join-Path $env:USERPROFILE "Downloads"
 		Write-Host " Launching EmuDeck Installer, please wait..."
 		&"$temp/emudeck_install.exe"
 	}
