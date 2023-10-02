1ec07678dfc1dd184cc547da9923263b8b97ff3c
Rodrigo <rodrigosedano@me.com>
2023-10-02 16:57:54 +0200

    early beta

--- functions/allCloud.ps1
+++ functions/allCloud.ps1
@@ -1,7 +1,1 @@
-. "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\vars.ps1"
-. "$env:USERPROFILE\EmuDeck\settings.ps1"
-. "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\functions\createLink.ps1"
-. "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\functions\createLauncher.ps1"
-. "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\functions\helperFunctions.ps1"
-. "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\functions\ToolScripts\emuDeckSaveSync.ps1"
-
+echo "NYI"
