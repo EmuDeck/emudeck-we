ed10f09c0152016850e88aece565298163730f70
Rodrigo <rodrigosedano@me.com>
2023-10-08 23:35:52 +0200

    RA saves paths fixes

--- functions/appImageInit.ps1
+++ functions/appImageInit.ps1
@@ -2,4 +2,24 @@ function appImageInit(){
 
 	setScreenDimensionsScale
 
+	$sourceFolder = "$savesPath/RetroArch/saves"
+	$destinationFolder = "$sourceFolder"
+
+	$subfolders = Get-ChildItem -Path $sourceFolder -Directory
+
+	foreach ($subfolder in $subfolders) {
+		$subfolderPath = $subfolder.FullName
+		Copy-Item -Path "$subfolderPath\*" -Destination $destinationFolder -Recurse
+	}
+
+	$sourceFolder = "$savesPath/RetroArch/states"
+	$destinationFolder = "$sourceFolder"
+
+	$subfolders = Get-ChildItem -Path $sourceFolder -Directory
+
+	foreach ($subfolder in $subfolders) {
+		$subfolderPath = $subfolder.FullName
+		Copy-Item -Path "$subfolderPath\*" -Destination $destinationFolder -Recurse
+	}
+
 }
