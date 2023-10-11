338c31b2708a2ec9155b3e442e2c8d733cf62532
Rodrigo <rodrigosedano@me.com>
2023-10-11 15:22:11 +0200

    faster init

--- functions/appImageInit.ps1
+++ functions/appImageInit.ps1
@@ -7,25 +7,27 @@ function appImageInit(){
 
 	$subfolders = Get-ChildItem -Path $sourceFolder -Directory
 
-	foreach ($subfolder in $subfolders) {
-		$subfolderPath = $subfolder.FullName
-		Copy-Item -Path "$subfolderPath\*" -Destination $destinationFolder -Recurse
+	if ($subfolders.Count -gt 0) {
+		foreach ($subfolder in $subfolders) {
+			$subfolderPath = $subfolder.FullName
+			Copy-Item -Path "$subfolderPath\*" -Destination $destinationFolder -Recurse
+		}
+
+		$sourceFolder = "$savesPath/RetroArch/states"
+		$destinationFolder = "$sourceFolder"
+
+		$subfolders = Get-ChildItem -Path $sourceFolder -Directory
+
+		foreach ($subfolder in $subfolders) {
+			$subfolderPath = $subfolder.FullName
+			Copy-Item -Path "$subfolderPath\*" -Destination $destinationFolder -Recurse
+		}
+
+		setConfigRA "sort_savefiles_by_content_enable" "false" $RetroArch_configFile
+		setConfigRA "sort_savefiles_enable" "false" $RetroArch_configFile
+		setConfigRA "sort_savestates_by_content_enable" "false" $RetroArch_configFile
+		setConfigRA "sort_savestates_enable" "false" $RetroArch_configFile
+		setConfigRA "sort_screenshots_by_content_enable" "false" $RetroArch_configFile
 	}
-
-	$sourceFolder = "$savesPath/RetroArch/states"
-	$destinationFolder = "$sourceFolder"
-
-	$subfolders = Get-ChildItem -Path $sourceFolder -Directory
-
-	foreach ($subfolder in $subfolders) {
-		$subfolderPath = $subfolder.FullName
-		Copy-Item -Path "$subfolderPath\*" -Destination $destinationFolder -Recurse
-	}
-
-	setConfigRA "sort_savefiles_by_content_enable" "false" $RetroArch_configFile
-	setConfigRA "sort_savefiles_enable" "false" $RetroArch_configFile
-	setConfigRA "sort_savestates_by_content_enable" "false" $RetroArch_configFile
-	setConfigRA "sort_savestates_enable" "false" $RetroArch_configFile
-	setConfigRA "sort_screenshots_by_content_enable" "false" $RetroArch_configFile
 
 }
