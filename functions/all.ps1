4159e0fdad1da0f00682556b1a1d0cc36fcac6c4
Rodrigo <rodrigosedano@me.com>
2023-10-18 22:08:44 +0200

    setLocation fix

--- functions/all.ps1
+++ functions/all.ps1
@@ -2,7 +2,11 @@
 . "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\vars.ps1"
 . "$env:USERPROFILE\EmuDeck\settings.ps1"
 
+if (-not $emulationPath) {
+	$emulationPath = "C:\"
+}
 Set-Location $emulationPath
+
 . "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\functions\download.ps1"
 . "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\functions\downloadCore.ps1"
 . "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\functions\showNotification.ps1"
