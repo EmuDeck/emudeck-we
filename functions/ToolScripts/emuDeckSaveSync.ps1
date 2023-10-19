c1915ce183281fa64a9705b8ed6492f69e29afee
Rodrigo <rodrigosedano@me.com>
2023-10-19 09:07:19 +0200

    Mindset Fix

--- functions/ToolScripts/emuDeckSaveSync.ps1
+++ functions/ToolScripts/emuDeckSaveSync.ps1
@@ -626,7 +626,7 @@ function cloud_sync_downloadEmu($emuName, $mode){
 
 function cloud_sync_createBackup($emuName){
  $date = Get-Date -Format "MM_dd_yyyy"
- Copy-Item -Path "$savesPath\$emuName" -Destination "$toolsPath\save-backups\$emuName" -Recurse
+ Copy-Item -Path "$savesPath\$emuName\*" -Destination "$toolsPath\save-backups\$emuName" -Recurse
  #We delete backups older than one month
  $oldDate = (Get-Date).AddDays(-30)
  Get-ChildItem -Path "$toolsPath\save-backups" -Directory | Where-Object { $_.CreationTime -lt $oldDate } | Remove-Item -Force -Recurse
