7ae1debe42e3dc88bb8821af4f4072785f7990e6
Rodrigo <rodrigosedano@me.com>
2023-10-19 09:21:30 +0200

    Revert "arrays"
    This reverts commit 82bef68ab644248ede9707012a178c8e6cd17286.

--- functions/helperFunctions.ps1
+++ functions/helperFunctions.ps1
@@ -108,12 +108,15 @@ function getLocations() {
 		}
 	}
 
+	$driveInfo += @{
+
+	}
 
 	# Ordenar el resultado por la propiedad 'letter'
 	$driveInfo = $driveInfo | Sort-Object letter
 
 	# Convertir el resultado en formato JSON
-	@($driveInfo) | ConvertTo-Json
+	$driveInfo | ConvertTo-Json
 }
 
 function customLocation(){
