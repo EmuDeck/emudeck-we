32ecfc0ebfeb13cc17688712f6d92221227ef2c3
Rodrigo <rodrigosedano@me.com>
2023-10-20 23:59:27 +0200

    Windows 10 Home detection

--- install.ps1
+++ install.ps1
@@ -3,7 +3,127 @@ clear
 $PSversionMajor = $PSVersionTable.PSVersion.Major
 $PSversionMinor = $PSVersionTable.PSVersion.Minor
 $PSversion = "$PSversionMajor$PSversionMinor"
+$osInfo = (systeminfo | findstr /B /C:"OS Name") | ForEach-Object { $_ -replace 'OS Name:', '' }
 
+
+
+Function NewWPFDialog() {
+	<#
+	.SYNOPSIS
+	This neat little function is based on the one from Brian Posey's Article on Powershell GUIs
+
+	.DESCRIPTION
+	  I re-factored a bit to return the resulting XaML Reader and controls as a single, named collection.
+
+	.PARAMETER XamlData
+	 XamlData - A string containing valid XaML data
+
+	.EXAMPLE
+
+	  $MyForm = New-WPFDialog -XamlData $XaMLData
+	  $MyForm.Exit.Add_Click({...})
+	  $null = $MyForm.UI.Dispatcher.InvokeAsync{$MyForm.UI.ShowDialog()}.Wait()
+
+	.NOTES
+	Place additional notes here.
+
+	.LINK
+	  http://www.windowsnetworking.com/articles-tutorials/netgeneral/building-powershell-gui-part2.html
+
+	.INPUTS
+	 XamlData - A string containing valid XaML data
+
+	.OUTPUTS
+	 a collection of WPF GUI objects.
+  #>
+
+	Param([Parameter(Mandatory = $True, HelpMessage = 'XaML Data defining a GUI', Position = 1)]
+		[string]$XamlData)
+
+	# Add WPF and Windows Forms assemblies
+	try {
+		Add-Type -AssemblyName PresentationCore, PresentationFramework, WindowsBase, system.windows.forms
+	}
+	catch {
+		Throw 'Failed to load Windows Presentation Framework assemblies.'
+	}
+
+	# Create an XML Object with the XaML data in it
+	[xml]$xmlWPF = $XamlData
+
+	# Create the XAML reader using a new XML node reader, UI is the only hard-coded object name here
+	Set-Variable -Name XaMLReader -Value @{ 'UI' = ([Windows.Markup.XamlReader]::Load((new-object -TypeName System.Xml.XmlNodeReader -ArgumentList $xmlWPF))) }
+
+	# Create hooks to each named object in the XAML reader
+	$Elements = $xmlWPF.SelectNodes('//*[@Name]')
+	ForEach ( $Element in $Elements ) {
+		$VarName = $Element.Name
+		$VarValue = $XaMLReader.UI.FindName($Element.Name)
+		$XaMLReader.Add($VarName, $VarValue)
+	}
+
+	return $XaMLReader
+}
+
+function confirmDialog {
+	param (
+		[string]$TitleText = "Do you want to continue?",
+		[string]$MessageText = "",
+		[string]$OKButtonText = "OK",
+		[string]$CancelButtonText = "Cancel"
+	)
+	# This is the XAML that defines the GUI.
+	$WPFXaml = @'
+<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+		xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+		Title="Popup" Background="#FF0066CC" Foreground="#FFFFFFFF" ResizeMode="NoResize" WindowStartupLocation="CenterScreen" SizeToContent="WidthAndHeight" WindowStyle="None" MaxWidth="600" Padding="20" Margin="0" Topmost="True">
+ <Grid Name="grid">
+			<ScrollViewer VerticalScrollBarVisibility="Auto" HorizontalScrollBarVisibility="Disabled">
+				<StackPanel>
+					<Border Margin="20,20,0,20" Background="Transparent">
+						<TextBlock Name="Title" Margin="0,10,0,10" TextWrapping="Wrap" Text="_TITLE_" FontSize="24" FontWeight="Bold" HorizontalAlignment="Left"/>
+					</Border>
+					<Border Margin="20,0,20,0" Background="Transparent">
+						<TextBlock Name="Message" Margin="0,0,0,20" TextWrapping="Wrap" Text="_CONTENT_" FontSize="18"/>
+					</Border>
+					<Border Margin="20,0,20,20" Background="Transparent">
+					<StackPanel Orientation="Horizontal" HorizontalAlignment="Right">
+						<Button Name="OKButton" Content="_OKBUTTONTEXT_" Margin="5" Width="75" Background="#FF0066CC" BorderBrush="White" Foreground="White" Padding="8,4"/>
+					</StackPanel>
+					</Border>
+				</StackPanel>
+			</ScrollViewer>
+		</Grid>
+</Window>
+'@
+
+	# Build Dialog
+	$WPFGui = NewWPFDialog -XamlData $WPFXaml
+	$WPFGui.Message.Text = $MessageText
+	$WPFGui.Title.Text = $TitleText
+	$WPFGui.Message.Text = $MessageText
+
+	$WPFGui.OKButton.Content = $OKButtonText
+
+	# Create a script block to handle the button click event
+	$buttonClickEvent = {
+		param($sender, $e)
+		$global:Result = $sender.Name
+		$WPFGui.UI.Close()
+	}
+
+	# Add the script block to the button's Click event
+	$WPFGui.OKButton.Add_Click($buttonClickEvent)
+
+	# Create a variable to hold the result
+	$global:Result = $null
+
+	# Show the dialog
+	$null = $WPFGUI.UI.Dispatcher.InvokeAsync{ $WPFGui.UI.ShowDialog() }.Wait()
+
+	# Return the result
+	return $global:Result
+}
 
 function getLatestReleaseURLGH($Repository, $FileType, $FindToMatch, $IgnoreText = "pepe"){
 
@@ -49,6 +169,16 @@ function download($url, $file) {
 	$wc.Downloadfile($url, $destination)
 
 	Write-Host "Done!" -NoNewline -ForegroundColor green -BackgroundColor black
+}
+
+if ($osInfo -contains "Windows 10 Home") {
+	$developerModeStatus = Get-WindowsDeveloperLicense
+	if ($developerModeStatus.DeveloperLicense) {
+		Write-Host "Developer Mode detected..."
+	} else {
+		confirmDialog -TitleText "Windows 10 Home Detected" -MessageText "You need to enable Developer mode inside your Windows Settings to install EmuDeck."
+		exit
+	}
 }
 
 if ( $PSversion -lt 51 ){
