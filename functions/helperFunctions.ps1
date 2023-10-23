cb0d4998901805a01e1d93e6b773017ce526b1fc
Rodrigo <rodrigosedano@me.com>
2023-10-23 11:04:02 +0200

    PSDrive.Name fix

--- functions/helperFunctions.ps1
+++ functions/helperFunctions.ps1
@@ -687,7 +687,7 @@ function createSaveLink($simLinkPath, $emuSavePath){
 		} else {
 			#Check if we have space
 
-			$userDrive=$emulationPath[0]
+			$userDrive=(Get-Item $emulationPath).PSDrive.Name
 			$destinationFree = (Get-PSDrive -Name $userDrive).Free
 			$sizeInGB = [Math]::Round($destinationFree / 1GB)
 
