9ee967f9cce01aa951e66dd84e8a7f6b057f06b2
Rodrigo <rodrigosedano@me.com>
2023-10-26 11:16:56 +0200

    autoFix function

--- functions/appImageInit.ps1
+++ functions/appImageInit.ps1
@@ -1,46 +1,13 @@
 function appImageInit(){
 
-	if ( "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\functions\allCloud.ps1"  -like "*NYI*"){
-		confirmDialog -TitleText "Corrupted installation" -MessageText "EmuDeck will reinstall after clicking OK, nothing will be deleted. This could take a few seconds to download"
-		$url_emudeck = getLatestReleaseURLGH 'EmuDeck/emudeck-electron-early' 'exe' 'emudeck'
-		download $url_emudeck "emudeck_install.exe"
-		&"$temp/emudeck_install.exe"
--ForegroundColor Cyan
-		break
-		exit
-	}
+	#AutoFixes
+	autofix_betaCorruption
+	autofix_cloudSyncLockfile
+	autofix_raSavesFolders
 
-    rm -fo "$userFolder\cloud.lock" -ErrorAction SilentlyContinue
-	SRM_init
+
+	# Init functions
 	setScreenDimensionsScale
-
-	$sourceFolder = "$savesPath/RetroArch/saves"
-	$destinationFolder = "$sourceFolder"
-
-	$subfolders = Get-ChildItem -Path $sourceFolder -Directory
-
-	if ($subfolders.Count -gt 0) {
-		foreach ($subfolder in $subfolders) {
-			$subfolderPath = $subfolder.FullName
-			Copy-Item -Path "$subfolderPath\*" -Destination $destinationFolder -Recurse
-		}
-
-		$sourceFolder = "$savesPath/RetroArch/states"
-		$destinationFolder = "$sourceFolder"
-
-		$subfolders = Get-ChildItem -Path $sourceFolder -Directory
-
-		foreach ($subfolder in $subfolders) {
-			$subfolderPath = $subfolder.FullName
-			Copy-Item -Path "$subfolderPath\*" -Destination $destinationFolder -Recurse
-		}
-
-		setConfigRA "sort_savefiles_by_content_enable" "false" $RetroArch_configFile
-		setConfigRA "sort_savefiles_enable" "false" $RetroArch_configFile
-		setConfigRA "sort_savestates_by_content_enable" "false" $RetroArch_configFile
-		setConfigRA "sort_savestates_enable" "false" $RetroArch_configFile
-		setConfigRA "sort_screenshots_by_content_enable" "false" $RetroArch_configFile
-	}
 
 	echo "true"
 
---
+++ functions/autofix.ps1
@@ -0,0 +1,54 @@
+#Put here your autofix functions, they should be load when EmuDeck Starts
+
+function autofix_betaCorruption(){
+	if ( "$env:USERPROFILE\AppData\Roaming\EmuDeck\backend\functions\allCloud.ps1"  -like "*NYI*"){
+			confirmDialog -TitleText "Corrupted installation" -MessageText "EmuDeck will reinstall after clicking OK, nothing will be deleted. This could take a few seconds to download"
+			$url_emudeck = getLatestReleaseURLGH 'EmuDeck/emudeck-electron-early' 'exe' 'emudeck'
+			download $url_emudeck "emudeck_install.exe"
+			&"$temp/emudeck_install.exe"
+	-ForegroundColor Cyan
+			break
+			exit
+		}
+}
+
+function autofix_cloudSyncLockfile(){
+	confirmDialog -TitleText "Corrupted installation" -MessageText "EmuDeck will reinstall after clicking OK, nothing will be deleted. This could take a few seconds to download"
+	rm -fo "$userFolder\cloud.lock" -ErrorAction SilentlyContinue
+}
+
+function autofix_raSavesFolders(){
+
+	$sourceFolder = "$savesPath/RetroArch/saves"
+	$destinationFolder = "$sourceFolder"
+	$subfolders = Get-ChildItem -Path $sourceFolder -Directory
+
+	if ($subfolders.Count -gt 0) {
+		cloud_sync_createBackup "retroarch"
+		confirmDialog -TitleText "Old RetroArch saves folders found" -MessageText "EmuDeck will create a backup of them in Emulation\saves-backup just in case, after that it will reorganize and delete the old subfolder. Please manually delete all subfolders you might have in your cloud provider ( EmuDeck/saves/retroarch/saves/* and EmuDeck/saves/retroarch/states/*)"
+		foreach ($subfolder in $subfolders) {
+			$subfolderPath = $subfolder.FullName
+			robocopy "$subfolderPath" "$destinationFolder" /E /XC /XN /XO
+			Remove-Item -Path $subfolderPath -Force -Recurse
+		}
+	}
+
+	$sourceFolder = "$savesPath/RetroArch/states"
+	$destinationFolder = "$sourceFolder"
+	$subfolders = Get-ChildItem -Path $sourceFolder -Directory
+
+	if ($subfolders.Count -gt 0) {
+		foreach ($subfolder in $subfolders) {
+			$subfolderPath = $subfolder.FullName
+			robocopy "$subfolderPath" "$destinationFolder" /E /XC /XN /XO
+			Remove-Item -Path $subfolderPath -Force -Recurse
+		}
+	}
+
+	setConfigRA "sort_savefiles_by_content_enable" "false" $RetroArch_configFile
+	setConfigRA "sort_savefiles_enable" "false" $RetroArch_configFile
+	setConfigRA "sort_savestates_by_content_enable" "false" $RetroArch_configFile
+	setConfigRA "sort_savestates_enable" "false" $RetroArch_configFile
+	setConfigRA "sort_screenshots_by_content_enable" "false" $RetroArch_configFile
+
+}
