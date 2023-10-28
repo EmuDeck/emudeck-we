cf15a60f48d2b18106c0e7b8bf4b0598c8e528ff
Rodrigo <rodrigosedano@me.com>
2023-10-29 01:33:43 +0200

    hiding notification script

--- functions/ToolScripts/emuDeckSaveSync.ps1
+++ functions/ToolScripts/emuDeckSaveSync.ps1
@@ -851,6 +851,37 @@ namespace CloseButtonToggle {
 				cls
 				$toast.Close()
 				invoke-expression 'cmd /c start /min powershell -Command {
+					$ShowWindowAsyncCode = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+					  $ShowWindowAsync = Add-Type -MemberDefinition $ShowWindowAsyncCode -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
+
+					  $hwnd = (Get-Process -PID $pid).MainWindowHandle
+					  if ($hwnd -ne [System.IntPtr]::Zero) {
+						# When you got HWND of the console window:
+						# (It would appear that Windows Console Host is the default terminal application)
+						$ShowWindowAsync::ShowWindowAsync($hwnd, 0)
+					  } else {
+						# When you failed to get HWND of the console window:
+						# (It would appear that Windows Terminal is the default terminal application)
+
+						# Mark the current console window with a unique string.
+						$UniqueWindowTitle = New-Guid
+						$Host.UI.RawUI.WindowTitle = $UniqueWindowTitle
+						$StringBuilder = New-Object System.Text.StringBuilder 1024
+
+						# Search the process that has the window title generated above.
+						$TerminalProcess = (Get-Process | Where-Object { $_.MainWindowTitle -eq $UniqueWindowTitle })
+						# Get the window handle of the terminal process.
+						# Note that GetConsoleWindow() in Win32 API returns the HWND of
+						# powershell.exe itself rather than the terminal process.
+						# When you call ShowWindowAsync(HWND, 0) with the HWND from GetConsoleWindow(),
+						# the Windows Terminal window will be just minimized rather than hidden.
+						$hwnd = $TerminalProcess.MainWindowHandle
+						if ($hwnd -ne [System.IntPtr]::Zero) {
+						  $ShowWindowAsync::ShowWindowAsync($hwnd, 0)
+						} else {
+						  Write-Host "Failed to hide the console window."
+						}
+					  }
 					. $env:USERPROFILE\AppData\Roaming\EmuDeck\backend\functions\all.ps1
 					echo "CloudSync: Waiting for the game to close. Keep this this window open!"
 					while($true){
