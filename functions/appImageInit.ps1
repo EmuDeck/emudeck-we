ce220040350107450b4f2495468f09527ce36c6b
Rodrigo <rodrigosedano@me.com>
2023-11-01 17:14:20 +0100

    autofix new settings

--- functions/appImageInit.ps1
+++ functions/appImageInit.ps1
@@ -1,12 +1,14 @@
 function appImageInit(){
 
 	#AutoFixes
-	#autofix_betaCorruption
+
+	autofix_betaCorruption
 	autofix_cloudSyncLockfile
 	autofix_raSavesFolders
 	autofix_lnk
 	autofix_ESDE
 	SRM_resetLaunchers
+	autofix_dynamicParsers
 	autofix_oldParsers
 
 	# Init functions
