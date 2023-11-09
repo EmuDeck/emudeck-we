3c50aa060bbcca804eef22e07b857ca5b9c3bf4f
Rodrigo <rodrigosedano@me.com>
2023-11-10 00:20:03 +0100

    cemu reorder

--- tools/launchers/cemu.ps1
+++ tools/launchers/cemu.ps1
@@ -1,8 +1,8 @@
+$emulatorFile = "$env:USERPROFILE/EmuDeck/EmulationStation-DE/Emulators/cemu/Cemu.exe"
+$scriptFileName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
 . "$env:USERPROFILE/AppData/Roaming/EmuDeck/backend/functions/allCloud.ps1"
 if($args){
 	$formattedArgs = $args | ForEach-Object { '"' + $_ + '"' }
 }
-$emulatorFile = "$env:USERPROFILE/EmuDeck/EmulationStation-DE/Emulators/cemu/Cemu.exe"
-$scriptFileName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
 
 emulatorInit $scriptFileName $emulatorFile ($formattedArgs -join ' ')
