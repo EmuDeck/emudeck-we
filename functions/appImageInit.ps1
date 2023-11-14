44f1f8e3695dae1344ed44bd2e20eab3427d5100
Rodrigo <rodrigosedano@me.com>
2023-11-14 10:11:18 +0100

    autofixBOM

--- functions/appImageInit.ps1
+++ functions/appImageInit.ps1
@@ -2,6 +2,17 @@ function appImageInit(){
 
 	#AutoFixes
 
+	#Remove SRM BOM
+	$userConfigsFile="$toolsPath\userData\userConfigurations.json"
+	$content = Get-Content -Path $userConfigsFile -Raw
+
+	# Crear un codificador para UTF-8 sin BOM
+	$killBOM = New-Object System.Text.UTF8Encoding $false
+
+	# Escribe el contenido de nuevo al archivo sin el BOM
+	[System.IO.File]::WriteAllText($userConfigsFile, $content, $killBOM)
+
+
 	autofix_betaCorruption
 	autofix_cloudSyncLockfile
 	autofix_raSavesFolders
