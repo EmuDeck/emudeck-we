5dc794d61d7943b524883e8755730e2b962a5953
Rodrigo <rodrigosedano@me.com>
2023-11-19 20:02:50 +0100

    Sed no BOM

--- functions/sedFile.ps1
+++ functions/sedFile.ps1
@@ -1,3 +1,6 @@
 function sedFile($file, $old, $new){
 	(Get-Content $file).replace($old, $new) | Set-Content $file -Encoding UTF8
+	$content = Get-Content -Path $file -Raw
+	$killBOM = New-Object System.Text.UTF8Encoding $false
+	[System.IO.File]::WriteAllText($file, $content, $killBOM)
 }
