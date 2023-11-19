6f505970b1ff6b8ccd8698a2a905275f0e013112
Rodrigo <rodrigosedano@me.com>
2023-11-19 23:44:57 +0100

    fullScreenToast

--- functions/helperFunctions.ps1
+++ functions/helperFunctions.ps1
@@ -911,17 +911,28 @@ function setScreenDimensionsScale(){
 function fullScreenToast {
 
 	[Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
 	$form = New-Object Windows.Forms.Form
 	$form.Text = "Popup"
 	$form.WindowState = [Windows.Forms.FormWindowState]::Maximized
 	$form.FormBorderStyle = [Windows.Forms.FormBorderStyle]::None
 	$form.BackColor = [System.Drawing.Color]::Black
+
+	# Obtener el tamaño de la pantalla
+	$screenWidth = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width
+	$screenHeight = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height
+
+	$form.Width = $screenWidth
+	$form.Height = $screenHeight
+
 	$pictureBox = New-Object Windows.Forms.PictureBox
-	$pictureBox.Image = [System.Drawing.Image]::FromFile("$env:USERPROFILE/AppData/Roaming/EmuDeck/backend/img/logo.png")  # Reemplaza "RutaDelGif.gif" con la ruta a tu GIF animado
+	$pictureBox.Image = [System.Drawing.Image]::FromFile("$env:USERPROFILE/AppData/Roaming/EmuDeck/backend/img/logo.png")
 	$pictureBox.SizeMode = [Windows.Forms.PictureBoxSizeMode]::CenterImage
-	$pictureBox.Dock = [Windows.Forms.DockStyle]::Fill  # Para que el PictureBox ocupe todo el formulario
+	$pictureBox.Dock = [Windows.Forms.DockStyle]::Fill
+
 	$form.Controls.Add($pictureBox)
 	$form.Show()
+
 	return $form
 }
 
