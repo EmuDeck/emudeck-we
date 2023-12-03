67b8f10fd9fd349a5ad10a78497421efff0647f2
Rodrigo <rodrigosedano@me.com>
2023-12-03 23:07:39 +0100

    gamemode fixes

--- functions/ToolScripts/emuDeckGameMode.ps1
+++ functions/ToolScripts/emuDeckGameMode.ps1
@@ -1,28 +1,22 @@
 function game_mode_enable(){
 	$source = "$userFolder\AppData\Roaming\EmuDeck\backend\tools\gamemode\start_game_mode.exe"
-	$test=Test-Path -Path "$userFolder\OneDrive\Desktop"
-	if(-not($test)){
-		$target =  "$userFolder\Desktop\Return to Gaming Mode.lnk"
-	}else{
-		$target =  "$userFolder\OneDrive\Desktop\Return to Gaming Mode.lnk"
-	}
-
+	$target =  "$userFolder\Desktop\Return to Gaming Mode.lnk"
 	$WScriptObj = New-Object -ComObject ("WScript.Shell")
 	$shortcut = $WscriptObj.CreateShortcut($target)
 	$shortcut.TargetPath = $source
 	$shortcut.WindowStyle = 7
-	$shortcut.IconLocation = "$userFolder\AppData\Roaming\EmuDeck\backend\tools\gamemode\icon.ico"
-	$shortcut.Save() -ErrorAction SilentlyContinue
+	$shortcut.IconLocation = "$env:APPDATA\EmuDeck\backend\tools\gamemode\icon.ico"
+	$shortcut.Save()
 
-	& "$userFolder\AppData\Roaming\EmuDeck\backend\tools\gamemode\enable.exe"
+	& "$env:APPDATA\EmuDeck\backend\tools\gamemode\enable.exe"
 	if($?){
 		Write-Output "true"
 	}
 }
 
 function game_mode_disable(){
-	 rm -fo -r "$env:USERPROFILE\Desktop\Return to Gaming Mode.lnk" -ErrorAction SilentlyContinue
-	& "$userFolder\AppData\Roaming\EmuDeck\backend\tools\gamemode\disable.exe"
+	rm -fo -r "$env:USERPROFILE\Desktop\Return to Gaming Mode.lnk" -ErrorAction SilentlyContinue
+	& "$env:APPDATA\EmuDeck\backend\tools\gamemode\disable.exe"
 	if($?){
 		Write-Output "true"
 	}
