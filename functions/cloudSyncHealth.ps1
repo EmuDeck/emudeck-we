f58a4c138147e329545d4d3e62e776ff5c8c3bfb
Dragoon Dorise <rodrigosedano@me.com>
2024-01-14 01:34:35 +0100

    cloudSyncHealth

---
+++ functions/cloudSyncHealth.ps1
@@ -0,0 +1,75 @@
+function cloud_sync_download_test($emuName){
+	if ((Test-Path "$cloud_sync_bin") -and ($cloud_sync_status -eq $true)) {
+
+		$target = "$emulationPath\saves\$emuName\"
+		if ( Test-Path "$target" ){
+			"test" | Set-Content "$target\.temp" -ErrorAction SilentlyContinue -Encoding UTF8
+			$fileHash = "$target\.temp"
+			Write-Host "Testing $emuName download..."
+			& $cloud_sync_bin -q --log-file "$userFolder/EmuDeck/logs/rclone.log" copyto --fast-list --checkers=50 --transfers=50 --low-level-retries 1 --retries 1 "$cloud_sync_provider`:Emudeck\saves\$emuName\.temp" "$fileHash"
+			if ($?) {
+				Write-Host ""
+			}else{
+				Write-Host "Error: Testing $emuName download"
+				rm -fo -r "$target\.temp" -ErrorAction SilentlyContinue
+				exit
+			}
+			rm -fo -r "$target\.temp" -ErrorAction SilentlyContinue
+		}
+
+	}
+}
+
+function cloud_sync_upload_test($emuNAme){
+	if ((Test-Path "$cloud_sync_bin") -and ($cloud_sync_status -eq $true)) {
+
+		$target = "$emulationPath\saves\$emuName\"
+		if ( Test-Path "$target" ){
+			"test" | Set-Content "$target\.temp" -ErrorAction SilentlyContinue -Encoding UTF8
+			$fileHash = "$target\.temp"
+
+			Write-Host "Testing $emuName upload..."
+
+			& $cloud_sync_bin -q --log-file "$userFolder/EmuDeck/logs/rclone.log" copyto --fast-list --checkers=50 --transfers=50 --low-level-retries 1 --retries 1 "$fileHash" "$cloud_sync_provider`:Emudeck\saves\$emuName\.temp"
+			if ($?) {
+				Write-Host ""
+			}else{
+				Write-Host "Error: Testing $emuName upload"
+				rm -fo -r "$target\.temp" -ErrorAction SilentlyContinue
+				exit
+			}
+			rm -fo -r "$target\.temp" -ErrorAction SilentlyContinue
+		}
+
+
+	}
+
+}
+
+function cloudSyncHealth(){
+
+	if ( ! Test-Path "$toolsPath\rclone\rclone.exe") {
+		Write-Host "nobinary"
+		exit
+	}
+	if ( ! Test-Path "$toolsPath\rclone\rclone.conf") {
+		Write-Host "noconfig"
+		exit
+	}
+	if ( $cloud_sync_provider -eq '') {
+		Write-Host "noprovider"
+		exit
+	}
+
+	$miArreglo = @("Cemu","citra","dolphin","duckstation","MAME","melonds","mgba","pcsx2","ppsspp","primehack","retroarch","rpcs3","scummvm","Vita3K","yuzu","ryujinx")
+
+	foreach ($elemento in $miArreglo) {
+		cloud_sync_upload_test $elemento
+	}
+
+	foreach ($elemento in $miArreglo) {
+		cloud_sync_download_test $elemento
+	}
+
+
+}
