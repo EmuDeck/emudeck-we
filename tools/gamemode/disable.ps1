1be68492b1cc1b1563e9321d46c36c0cd753b3f2
Dragoon Dorise <rodrigosedano@me.com>
2024-02-06 10:54:05 +0100

    "

--- tools/gamemode/disable.ps1
+++ tools/gamemode/disable.ps1
@@ -1,5 +1,5 @@
 . "$env:APPDATA\EmuDeck\backend\functions/all.ps1"
 $scriptContent = @"
-	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "Shell" -Value "explorer.exe
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "Shell" -Value "explorer.exe"
 "@
 startScriptWithAdmin -ScriptContent $scriptContent
