4a55439061f93c83c5dd5ce54d9f46a906865c04
Dragoon Dorise <rodrigosedano@me.com>
2024-02-06 10:50:17 +0100

    Fix boot mode back to desktop

--- tools/gamemode/disable.ps1
+++ tools/gamemode/disable.ps1
@@ -2,4 +2,5 @@
 $scriptContent = @"
 	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "Shell" -Value "explorer.exe
 "@
+startScriptWithAdmin -ScriptContent $scriptContent
 confirmDialog -TitleText "Desktop Mode" -MessageText "Next time you restart your computer, Game Mode will be OFF"
