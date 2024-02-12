98674d98e80a82acbc8d2fb2108eda972c3b0efe
Dragoon Dorise <rodrigosedano@me.com>
2024-02-12 16:40:42 +0100

    setup rest of emulators

--- android/setup.ps1
+++ android/setup.ps1
@@ -50,18 +50,62 @@ Start-Sleep 1
 setMSG "Installing, please stand by..."
 rm -fo -r $env:USERPROFILE/EmuDeck/android/temp
 Write-Output ""
+$copySavedGames="false"
+$result = yesNoDialog -TitleText "Saved Games" -MessageText "Do you want us to copy your current saved games to your Android Device?" -OKButtonText "Yes" -CancelButtonText "No"
+
+if ($result -eq "OKButton") {
+	$copySavedGames="true"
+} else {
+	$copySavedGames="false"
+}
 
 #Roms folders
 if ( $android_writable -eq "true" ){
 	setMSG "Creating rom folders in $androidStoragePath..."
 	Android_ADB_push "$env:APPDATA\EmuDeck\backend\android\roms" "$androidStoragePath/Emulation/roms"
+	setMSG "Copying BIOS"
+	Android_ADB_push "$biosPath" "$androidStoragePath/Emulation/bios"
+
+	if ($copySavedGames -eq "true") {
+		setMSG "Copying Saves & States"
+		#RA
+		Android_ADB_push "$savesPath/RetroArch" "$androidStoragePath/Emulation/saves/RetroArch/"
+		#PPSSPP
+		Android_ADB_push "$emusPath\PPSSPP\memstick\PSP\SAVEDATA"  "$androidStoragePath/Emulation/saves/PSP/SAVEDATA"
+		Android_ADB_push "$emusPath\PPSSPP\memstick\PSP\PPSSPP_STATE" "$androidStoragePath/Emulation/saves/PSP/PPSSPP_STATE"
+		#Yuzu
+		Android_ADB_push "$savesPath/yuzu/saves/" "/storage/emulated/0/Android/data/org.yuzu.yuzu_emu/files/nand/user/saves/"
+		#Citra
+		Android_ADB_push "$savesPath\citra\saves" "/storage/emulated/0/citra-emu/sdmc/"
+
+	}
+
 }else{
+
 	if ( $androidStoragePath -like "*-*" ){
-		copyFromTo "$env:APPDATA\EmuDeck\backend\android\roms" "$Android_temp_external/Emulation/roms"
+		$Android_cond_path=$Android_temp_external
 	}else{
-		copyFromTo "$env:APPDATA\EmuDeck\backend\android\roms" "$Android_temp_internal/Emulation/roms"
+		$Android_cond_path=$Android_temp_internal
+	}
+	setMSG "Creating rom folders in $androidStoragePath..."
+	copyFromTo "$env:APPDATA\EmuDeck\backend\android\roms" "$Android_cond_path/Emulation/roms"
+	setMSG "Copying BIOS"
+	copyFromTo "$biosPath" "$Android_cond_path/Emulation/bios"
+
+	if ($copySavedGames -eq "true") {
+		setMSG "Copying Saves & States"
+		#RA
+		copyFromTo "$savesPath/RetroArch" "$Android_cond_path/Emulation/saves/RetroArch/"
+		#PPSSPP
+		Android_ADB_push "$savesPath\ppsspp\saves"  "$androidStoragePath/Emulation/saves/PSP/SAVEDATA"
+		Android_ADB_push "$savesPath\ppsspp\states" "$androidStoragePath/Emulation/saves/PSP/PPSSPP_STATE"
+		#Yuzu
+		copyFromTo "$savesPath/yuzu/saves/" "$Android_temp_internal/Android/data/org.yuzu.yuzu_emu/files/nand/user/saves/"
+		#Citra
+		copyFromTo "$savesPath\citra\saves" "$Android_temp_internal/citra-emu/sdmc/"
 	}
 }
+
 
 
 Android_Pegasus_install
@@ -72,7 +116,7 @@ Android_RetroArch_install
 Android_PPSSPP_install
 Android_Yuzu_install
 Android_ScummVM_install
-Android_Vita3K_install
+#Android_Vita3K_install
 
 
 Android_Pegasus_init
@@ -83,11 +127,12 @@ Android_PPSSPP_init
 Android_Yuzu_init
 Android_ScummVM_init
 Android_Vita3K_init
-Android_RetroArch_init
+#Android_RetroArch_init
 
 
 if ( $android_writable -eq "false" ){
-	  setMSG "Moving settings and roms folder ussing MTP, expect some pop ups behind this window"
+	  setMSG "Moving data ussing MTP, expect some pop ups behind this window"
+
 	  Move-To-MTP -parent "CopyToInternal" -path "Internal shared storage"
 	  if ( $androidStoragePath -like "*-*" ){
 		  $phone = Get-Phone
@@ -95,11 +140,48 @@ if ( $android_writable -eq "false" ){
 		  $SDCARDNAME = $SDObject.Name
 		  Move-To-MTP -parent "CopyToSDCARD" -path "$SDCARDNAME"
 	  }
+	  confirmDialog -TitleText "Wait" -MessageText "Please wait for all transfers for complete"
 }
 
-setMSG "Please close EmuDeck when the transfers are completed"
-setMSG "100 #ANDROID"
-confirmDialog -TitleText "RetroArch Cores" -MessageText "Don't close this pop up until all transfers have finished in the background"
-Start-Sleep 1000
 
+#adb shell 'dumpsys package | grep -Eo "^[[:space:]]+[0-9a-f]+[[:space:]]+org.dolphinemu.mmjr/[^[:space:]]+" | grep -oE "[^[:space:]]+$"'
+
+#Citra setup.
+adb shell am start -n org.citra.emu/.ui.MainActivity
+confirmDialog -TitleText "Citra" -MessageText "Go to your device, and click on allow access. Press Continue when you've done it."
+adb shell am force-stop org.citra.emu
+
+#Dolphin setup
+adb shell am start -n org.dolphinemu.mmjr/org.dolphinemu.dolphinemu.ui.main.MainActivity
+confirmDialog -TitleText "Dolphin" -MessageText "Go to your device, and click on allow access. Press Continue when you've done it."
+adb shell am force-stop org.dolphinemu.mmjr
+
+#Yuzu setup
+adb shell am start -n org.yuzu.yuzu_emu/.ui.main.MainActivity
+confirmDialog -TitleText "Yuzu" -MessageText "Go to your device, and click on Get Started and finish setup on your device. Your Keys should be in $androidStorage/Emulation/bios/yuzu/keys. Press Continue when you've done it."
+adb shell am force-stop org.yuzu.yuzu_emu
+
+#PPSSPP setup
+adb shell am start -n org.ppsspp.ppsspp/.PpssppActivity
+confirmDialog -TitleText "PPSSPP" -MessageText "Go to your device, and click on allow access, create the PSP folder in $androidStorage/Emulation/saves/. Press Continue when you've done it."
+adb shell am force-stop org.ppsspp.ppsspp
+
+#AetherSX2 setup
+adb shell am start -n xyz.aethersx2.android/.MainActivity
+confirmDialog -TitleText "AetherSX2" -MessageText "Go to your device, and click on allow access and setup your bios that should be in $androidStorage/Emulation/bios. Press Continue when you've done it."
+adb shell am force-stop xyz.aethersx2.android
+
+#Scummvm setup
+adb shell am start -n org.scummvm.scummvm/.SplashActivity
+confirmDialog -TitleText "Scummvm" -MessageText "Go to your device, and click on allow access. Press Continue when you've done it."
+adb shell am force-stop org.scummvm.scummvm
+
+#RetroArch setup
+adb shell am start -n com.retroarch.aarch64/com.retroarch.browser.mainmenu.MainMenuActivity
+confirmDialog -TitleText "RetroArch" -MessageText "Go to your device, click on allow access, go to Load Core, Install or Restore a Core and install all the cores you need. Then go to Settings, Directory. Set your System/Bios to $androidStorage/Emulation/bios. Set Save Files to $androidStorage/Emulation/saves/RetroArch/saves and Save States to $androidStorage/Emulation/saves/RetroArch/states"
+adb shell am force-stop com.retroarch.aarch64
+
+
+
+setMSG "100 #ANDROID"
 Stop-Transcript
