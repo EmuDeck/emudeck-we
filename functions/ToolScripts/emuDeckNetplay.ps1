464bfe81d4de8a0e67d6e4acc137e84869a88fc7
Dragoon Dorise <rodrigosedano@me.com>
2024-03-25 23:47:29 +0100

    netplaySetIP

---
+++ functions/ToolScripts/emuDeckNetplay.ps1
@@ -0,0 +1,21 @@
+function netplaySetIP(){
+	$localIP = (Get-NetIPAddress | Where-Object {$_.AddressFamily -eq "IPv4" -and $_.InterfaceAlias -ne "Loopback"}).IPAddress[0]
+	$IPParts = $localIP.Split(".")
+	$segment = $IPParts[0] + "." + $IPParts[1] + "." + $IPParts[2]
+	$subnet = "$segment."
+	$port = 55435
+	$nethost = "none"
+	setSetting netplayIP "false"
+	1..254 | ForEach-Object {
+		$ip = $subnet + $_
+		$tcpClient = New-Object System.Net.Sockets.TcpClient
+		$tcpClient.SendTimeout = 2
+		$tcpClient.ReceiveTimeout = 2
+		$result = $tcpClient.BeginConnect($ip, $port, $null, $null)
+		$wait = $result.AsyncWaitHandle.WaitOne(1, $false)
+		if ($wait -eq $true -and $tcpClient.Connected) {
+			setSetting netplayIP $ip
+		}
+		$tcpClient.Close()
+	}
+}
