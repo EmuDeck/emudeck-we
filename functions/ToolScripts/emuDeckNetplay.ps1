8beeb20c2c852365f50ce5583c23f9ab44237135
Dragoon Dorise <rodrigosedano@me.com>
2024-04-14 17:02:12 +0200

    netplaysetip changes

--- functions/ToolScripts/emuDeckNetplay.ps1
+++ functions/ToolScripts/emuDeckNetplay.ps1
@@ -1,21 +1,21 @@
 function netplaySetIP(){
-	$localIP = (Get-NetIPAddress | Where-Object {$_.AddressFamily -eq "IPv4" -and $_.InterfaceAlias -ne "Loopback"}).IPAddress[0]
+
+	$activeInterfaceIndex = Get-NetRoute -DestinationPrefix "0.0.0.0/0" | Sort-Object -Property Metric | Select-Object -First 1 -ExpandProperty InterfaceIndex
+
+	$ipv4Address = Get-NetIPConfiguration | Where-Object {$_.InterfaceIndex -eq $activeInterfaceIndex} | Select-Object -ExpandProperty IPv4Address
+	$localIP = $ipv4Address.IPAddress
 	$IPParts = $localIP.Split(".")
 	$segment = $IPParts[0] + "." + $IPParts[1] + "." + $IPParts[2]
 	$subnet = "$segment."
-	$port = 55435
-	2..255 | ForEach-Object {
-		$ip = $subnet + $_
-		$tcpClient = New-Object System.Net.Sockets.TcpClient
-		$tcpClient.SendTimeout = 1
-		$tcpClient.ReceiveTimeout = 1
-		$result = $tcpClient.BeginConnect($ip, $port, $null, $null)
-		$wait = $result.AsyncWaitHandle.WaitOne(1, $false)
 
-		if ($wait -eq $true -and $tcpClient.Connected) {
+
+	for ($i = 2; $i -lt 255; $i++) {
+		$ip = "$subnet$i"
+		$isConnected = [System.Net.Sockets.TcpClient]::new().ConnectAsync($ip, 55435).Wait(50)
+
+ 	   if ($isConnected) {
 			setSetting "netplayCMD" "-C $ip"
 			return $ip
 		}
-		$tcpClient.Close()
 	}
 }
