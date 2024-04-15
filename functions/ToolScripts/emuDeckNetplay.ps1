f280f7da05783041a3f4b0b712799697dfae3eee
Dragoon Dorise <rodrigosedano@me.com>
2024-04-15 17:09:37 +0200

    updated netplay

--- functions/ToolScripts/emuDeckNetplay.ps1
+++ functions/ToolScripts/emuDeckNetplay.ps1
@@ -7,15 +7,35 @@ function netplaySetIP(){
 	$IPParts = $localIP.Split(".")
 	$segment = $IPParts[0] + "." + $IPParts[1] + "." + $IPParts[2]
 	$subnet = "$segment."
+	$port = 55435
+	2..255 | ForEach-Object {
+		$ip = "$segment.$_"
+		Write-Host $ip  # Imprimir la IP que se está probando
 
-
-	for ($i = 2; $i -lt 255; $i++) {
-		$ip = "$subnet$i"
-		$isConnected = [System.Net.Sockets.TcpClient]::new().ConnectAsync($ip, 55435).Wait(50)
-
- 	   if ($isConnected) {
-			setSetting "netplayCMD" "-C $ip"
-			return $ip
+		$ping = New-Object System.Net.NetworkInformation.Ping
+		try {
+			$result = $ping.Send($ip, 1)  # 5 ms de tiempo de espera
+			if ($result.Status -eq 'Success') {
+				"Host is reachable."
+				try {
+					$tcpclient = New-Object System.Net.Sockets.TcpClient
+					$tcpclient.Connect("$ip", $port)
+					if ($tcpclient.Connected) {
+						"Port $port is open."
+						setSetting "netplayCMD" "-C $ip"
+						break
+					}
+					$tcpclient.Close()
+				} catch {
+					"Port $port is closed or unreachable."
+				}
+			} else {
+				"Host is unreachable."
+			}
+		} catch {
+			"Failed to ping due to an error."
 		}
+
 	}
+
 }
