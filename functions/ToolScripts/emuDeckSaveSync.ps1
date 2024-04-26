da08aa1da384ac9b2ea0b76e756805d58c3eb14d
Dragoon Dorise <rodrigosedano@me.com>
2024-04-26 22:29:32 +0200

    no token check

--- functions/ToolScripts/emuDeckSaveSync.ps1
+++ functions/ToolScripts/emuDeckSaveSync.ps1
@@ -393,32 +393,32 @@ function cloud_sync_download($emuName){
 	if ((Test-Path "$cloud_sync_bin") -and ($cloud_sync_status -eq $true)) {
 
 
-		& "$cloud_sync_bin"  --progress copyto -L --fast-list --checkers=50 --transfers=50 --low-level-retries 1 --retries 1 "$cloud_sync_provider`:Emudeck/saves/.token" "$savesPath/.token"
-
-		$tokenPath = "$savesPath/.token"
-		if (Test-Path $tokenPath) {
-			# Lee el token desde el archivo
-			$token = Get-Content $tokenPath
-
-			$url = "https://token.emudeck.com/quick-check.php?access_token=$token"
-
-			$response = Invoke-RestMethod -Uri $url
-
-
-			if ($response.status -eq $true) {
-				Write-Host "Continue"
-			}
-			else {
-				echo "exit"
-				#confirmDialog -TitleText "Outdated token" -MessageText "Please open EmuDeck to regenerate your token"
-				#exit
-			}
-		} else {
-			echo "exit"
-			#confirmDialog -TitleText "Token not found" -MessageText "Please open EmuDeck to regenerate your token"
-			Write-Host "Token not found: $tokenPath"
-			#exit
-		}
+# 		& "$cloud_sync_bin"  --progress copyto -L --fast-list --checkers=50 --transfers=50 --low-level-retries 1 --retries 1 "$cloud_sync_provider`:Emudeck/saves/.token" "$savesPath/.token"
+#
+# 		$tokenPath = "$savesPath/.token"
+# 		if (Test-Path $tokenPath) {
+# 			# Lee el token desde el archivo
+# 			$token = Get-Content $tokenPath
+#
+# 			$url = "https://token.emudeck.com/quick-check.php?access_token=$token"
+#
+# 			$response = Invoke-RestMethod -Uri $url
+#
+#
+# 			if ($response.status -eq $true) {
+# 				Write-Host "Continue"
+# 			}
+# 			else {
+# 				echo "exit"
+# 				#confirmDialog -TitleText "Outdated token" -MessageText "Please open EmuDeck to regenerate your token"
+# 				#exit
+# 			}
+# 		} else {
+# 			echo "exit"
+# 			#confirmDialog -TitleText "Token not found" -MessageText "Please open EmuDeck to regenerate your token"
+# 			Write-Host "Token not found: $tokenPath"
+# 			#exit
+# 		}
 
 		#We wait for any upload in progress
 		cloud_sync_check_lock
