9c564dc0f12312db8a29ce953a3e979daf9082dd
Dragoon Dorise <rodrigosedano@me.com>
2024-06-12 14:13:07 +0200

    Revert "Lime3DS Android"
    This reverts commit 76f4c5446538241f6b1c2e07e560e62ead1ceab1.

--- android/functions/EmuScripts/Android_Citra.ps1
+++ android/functions/EmuScripts/Android_Citra.ps1
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-function Android_Lime3DS_install(){
-	setMSG "Installing Lime3DS"
-	$temp_url="$(Android_getLatestReleaseURLGH "Lime3DS/Lime3DS" ".apk")"
+function Android_Citra_install(){
+	setMSG "Installing Citra"
+	$temp_url="$(Android_getLatestReleaseURLGH "weihuoya/citra" ".apk")"
 	$temp_emu="citra"
 	Android_ADB_dl_installAPK $temp_emu $temp_url
 }
 
-function Android_Lime3DS_init(){
+function Android_Citra_init(){
 	setMSG "Setting up Citra"
 
 	copyFromTo "$env:APPDATA/EmuDeck/backend/android/configs/citra-emu/" "$Android_temp_internal/citra-emu/"
@@ -15,14 +15,14 @@ function Android_Lime3DS_init(){
 	Android_ADB_push "$Android_temp_internal/citra-emu/" /storage/emulated/0/citra-emu/
 }
 
-function Android_Lime3DS_setup(){
+function Android_Citra_setup(){
 	adb shell pm grant org.citra.emu android.permission.WRITE_EXTERNAL_STORAGE
 	adb shell am start -n org.citra.emu/.ui.MainActivity
 	Start-Sleep -Seconds 1
 	adb shell am force-stop org.citra.emu
 }
 
-function Android_Lime3DS_IsInstalled(){
+function Android_Citra_IsInstalled(){
 	$package="org.citra.emu"
 	$test= adb shell pm list packages $package
 	if ($test){
