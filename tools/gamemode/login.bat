1c484eae2342b7d99b60cb60cbf757f75fe4b806
Dragoon Dorise <rodrigosedano@me.com>
2024-09-12 23:34:58 +0200

    Revert "delayed explorer.exe"
    This reverts commit afca8e4c1967be19f6c6091fa8c0ba8de2806c02.

--- tools/gamemode/login.bat
+++ tools/gamemode/login.bat
@@ -1,2 +1,1 @@
-start "" powershell -ExecutionPolicy Bypass -NoExit . "%AppData%\EmuDeck\backend\tools\gamemode\login.ps1"
-start "" explorer.exe
+powershell -ExecutionPolicy Bypass -NoExit . "%AppData%\EmuDeck\backend\tools\gamemode\login.ps1"
