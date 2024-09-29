f45080692551f36b9a2611a3ff1093668b80f076
Dragoon Dorise <rodrigosedano@me.com>
2024-09-30 00:01:49 +0200

    Start-Process

--- functions/ToolScripts/emuDeckSaveSync.ps1
+++ functions/ToolScripts/emuDeckSaveSync.ps1
@@ -269,7 +269,7 @@ function cloud_sync_config($cloud_sync_provider, $token){
 
 
 
-		 $pass= $($password.cloud_token)
+		 $ofuspass= $($password.cloud_token)
 		 $user=$($parts[0])
 		 setSetting "cs_user" "cs_$user\"
 
@@ -277,7 +277,9 @@ function cloud_sync_config($cloud_sync_provider, $token){
 			createCloudFile $_.FullName
 		 }
 
-		 & "$cloud_sync_bin" config update Emudeck-cloud host=cloud.emudeck.com user=cs_$user port=22 pass="$pass"
+		 Start-Process $cloud_sync_bin -ArgumentList @"
+		 config update Emudeck-cloud host=cloud.emudeck.com user=cs_$user port=22 pass="$ofuspass"
+"@  -WindowStyle Maximized -Wait
 		 & $cloud_sync_bin mkdir "$cloud_sync_provider`:$cs_user`Emudeck\saves"
 		 & $cloud_sync_bin copy $savesPath "$cloud_sync_provider`:$cs_user`Emudeck\saves" --include "*.cloud"
 		 #Cleaning up
