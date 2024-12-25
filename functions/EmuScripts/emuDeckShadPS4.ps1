f46c7568a5cd8057aed49e17444026fd55be616d
Dragoon Dorise <rodrigosedano@me.com>
2024-12-26 00:43:19 +0100

    -qt

--- functions/EmuScripts/emuDeckShadPS4.ps1
+++ functions/EmuScripts/emuDeckShadPS4.ps1
@@ -11,7 +11,7 @@ function ShadPS4_init(){
 	setMSG "ShadPS4 - Configuration"
 	$destination="$emusPath\ShadPS4\user"
 	mkdir $destination -ErrorAction SilentlyContinue
-	copyFromTo "$env:APPDATA\EmuDeck\backend\configs\ShadPS4" "$destination"
+	copyFromTo "$env:APPDATA\EmuDeck\backend\configs\ShadPS4-qt" "$destination"
 	#ShadPS4_setResolution $ShadPS4Resolution
 	ShadPS4_setupStorage
 	#ShadPS4_setupSaves
