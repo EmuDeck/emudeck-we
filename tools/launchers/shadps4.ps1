84fec09a0a364213a582790990c901d8110d8cf6
Dragoon Dorise <rodrigosedano@me.com>
2024-12-26 01:02:06 +0100

    launcher proper path?

--- tools/launchers/shadps4.ps1
+++ tools/launchers/shadps4.ps1
@@ -1,3 +1,4 @@
+cd "$env:USERPROFILE/EmuDeck/Emulators/shadps4-qt/"
 $emulatorFile = "$env:USERPROFILE/EmuDeck/Emulators/shadps4-qt/shadps4.exe"
 $scriptFileName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
 . "$env:USERPROFILE/AppData/Roaming/EmuDeck/backend/functions/allCloud.ps1"
