8598b122f30284a9a8828cb5157a9fc3809f9908
Dragoon Dorise <rodrigosedano@me.com>
2024-12-28 19:11:19 +0100

    moveFromTo force

--- functions/moveFromTo.ps1
+++ functions/moveFromTo.ps1
@@ -1,5 +1,3 @@
-function moveFromTo($old,$new){
-	robocopy "$old" $new /s /Move /NFL /NDL /NJH /NJS /nc /ns /np
+function moveFromTo($old, $new){
+	robocopy "$old" $new /s /Move /IS /NFL /NDL /NJH /NJS /nc /ns /np
 }
-
-
