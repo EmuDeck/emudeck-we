b0eb9f19e7646625afffdeccb28ca4bae60802e8
Dragoon Dorise <rodrigosedano@me.com>
2024-12-30 23:15:47 +0100

    migration

--- functions/appImageInit.ps1
+++ functions/appImageInit.ps1
@@ -1,30 +1,51 @@
 function appImageInit(){
 
-	#Folder Migration
+
+	#emudeck folder relocation
 	$path = "$env:USERPROFILE/EmuDeck"
 	if (Test-Path -Path $path -PathType Container) {
 
-		$item = Get-Item $path
+	   confirmDialog -TitleText "Migration" -MessageText "We are going to move the $env:USERPROFILE/EmuDeck folder to $env:APPDATA\EmuDeck.Please wait until a new message confirms the migration"
 
-		if ($item.Attributes -band [System.IO.FileAttributes]::ReparsePoint) {
-			Write-Output "$path it's a junction."
-		} else {
+		$destination = "$env:APPDATA/EmuDeck"
+			# We copy the saves to the Emulation/saves Folder and we create a backup
+		echo "Creating saves symlink"
+		$originalFolderName = Split-Path $path -Leaf
+		$newFolderName = Split-Path $destination -Leaf
 
-			Get-ChildItem -Path "$env:USERPROFILE/EmuDeck" -Recurse -Directory | ForEach-Object {
-				if ((Get-Item $_.FullName).Attributes -band [System.IO.FileAttributes]::ReparsePoint) {
-					Write-Host "Eliminando junction: $($_.FullName)" -ForegroundColor Yellow
-					Remove-Item -Path $_.FullName -Recurse -Force
-				}
+		Get-ChildItem -Path "$path" | ForEach-Object {
+			$destinationPath = Join-Path -Path $destination -ChildPath $_.Name
+			if (-Not (Test-Path -Path $destinationPath)) {
+			   Write-Host "Moving: $_.FullName to $destination" -ForegroundColor Blue
+			   Move-Item -Path $_.FullName -Destination $destination
+			} else {
+				Write-Host "El elemento ya existe en el destino: $destinationPath" -ForegroundColor Yellow
 			}
-			#Write-Output "$path it's a directory."
-			confirmDialog -TitleText "Migration" -MessageText "We are going to move the $env:USERPROFILE/EmuDeck folder to $env:APPDATA\EmuDeck.Please wait until a new message confirms the migration"
-
-			moveFromTo "$path" "$env:APPDATA\EmuDeck"
-			createSaveLink $path "$env:APPDATA\EmuDeck"
-			SRM_resetLaunchers
-
-			confirmDialog -TitleText "Complete" -MessageText "Migration complete,you can now use EmuDeck as always. The Emulation folder is still at $emulationPath"
 		}
+
+		#createSaveLink $path "$env:APPDATA\EmuDeck\"
+
+		 . "$env:APPDATA/EmuDeck/settings.ps1"
+		 . "$env:APPDATA/EmuDeck/backend/vars.ps1"
+
+		 SRM_resetLaunchers
+
+		 Remove-Item -Path $path -Force
+
+		 confirmDialog -TitleText "Complete" -MessageText "Migration complete,you can now use EmuDeck as always. The Emulation folder is still at $emulationPath"
+	 }
+
+	 $path = "$esdePath/Emulators"
+	 if (Test-Path -Path $path -PathType Container) {
+	  $item = Get-Item $path
+	  if ($item.Attributes -band [System.IO.FileAttributes]::ReparsePoint) {
+		Write-Output "$path it's a junction."
+	  } else {
+		Write-Output "$path it's a directory."
+		Move-Item -Path $path -Destination $emusPath
+		createSaveLink $path $emusPath
+		SRM_resetLaunchers
+	  }
 	}
 
 	#AutoFixes
