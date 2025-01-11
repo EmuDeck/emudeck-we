8b390ded7ddea6c78b0b951fa2349ea3b8d17b68
Dragoon Dorise <rodrigosedano@me.com>
2025-01-11 13:08:18 +0100

    \AppData\Roaming\EmuDeck data fixes

--- tools/cloudSync/cloud_sync_watcher.ps1
+++ tools/cloudSync/cloud_sync_watcher.ps1
@@ -1,7 +1,7 @@
 $user=$args[0]
 $userPath = ( Get-CimInstance Win32_UserProfile -Filter "SID = '$((Get-LocalUser $user).Sid)'" ).LocalPath
-$logPath = Join-Path -Path "$userPath" -ChildPath 'EmuDeck\logs\cloudWatcher.log'
-$f1 = Join-Path -Path "$userPath" -ChildPath 'EmuDeck\settings.ps1'
+$logPath = Join-Path -Path "$userPath" -ChildPath '\AppData\Roaming\EmuDeck\logs\cloudWatcher.log'
+$f1 = Join-Path -Path "$userPath" -ChildPath '\AppData\Roaming\EmuDeck\settings.ps1'
 $f2 = Join-Path -Path "$userPath" -ChildPath 'AppData\Roaming\EmuDeck\backend\functions\createLink.ps1'
 $f3 = Join-Path -Path "$userPath" -ChildPath 'AppData\Roaming\EmuDeck\backend\functions\createLauncher.ps1'
 $f4 = Join-Path -Path "$userPath" -ChildPath 'AppData\Roaming\EmuDeck\backend\functions\helperFunctions.ps1'
