84acaaa40104f735332f5025e2cd6ab9fc8c1638
Dragoon Dorise <rodrigosedano@me.com>
2025-02-08 22:47:09 +0100

    fix bigpemu launcher

--- tools/launchers/BigPEmu.ps1
+++ tools/launchers/BigPEmu.ps1
@@ -1,4 +1,4 @@
-$emulatorFile = "$env:USERPROFILE/EmuDeck/Emulators/BigPEmu/BigPEmu.exe"
+$emulatorFile = "$env:APPDATA/EmuDeck/Emulators/BigPEmu/BigPEmu.exe"
 $scriptFileName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
 . "$env:USERPROFILE/AppData/Roaming/EmuDeck/backend/functions/allCloud.ps1"
 if($args){
