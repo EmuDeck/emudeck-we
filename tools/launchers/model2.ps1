2f5ff0d17e26065a31c19d8fdf220f8e0e3fcd8d
Dragoon Dorise <rodrigosedano@me.com>
2025-02-10 15:30:11 +0100

    model2 cloud

--- tools/launchers/model2.ps1
+++ tools/launchers/model2.ps1
@@ -1,3 +1,14 @@
 cd "$env:APPDATA/EmuDeck/Emulators/Model2"
 $emulatorFile = "./EMULATOR.exe"
+$emuName = "model2"
+
+. "$env:USERPROFILE/AppData/Roaming/EmuDeck/backend/functions/allCloud.ps1"
+hideMe
+isLatestVersionGH($emuName)
+checkAndStartSteam
+cloud_sync_init($emuName)
+
 & $emulatorFile $args
+
+rm -fo -r "$savesPath/.watching" -ErrorAction SilentlyContinue
+rm -fo -r "$savesPath/.emulator" -ErrorAction SilentlyContinue
