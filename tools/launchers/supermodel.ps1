88c974ab23b50bffb2f62707e86d1838d93ebe32
Dragoon Dorise <rodrigosedano@me.com>
2025-02-10 13:54:59 +0100

    supermodel fix

--- tools/launchers/supermodel.ps1
+++ tools/launchers/supermodel.ps1
@@ -1,3 +1,4 @@
+cd "$env:APPDATA/EmuDeck/Emulators/Supermodel"
 $emulatorFile = "$env:APPDATA/EmuDeck/Emulators/Supermodel/Supermodel.exe"
 $scriptFileName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
 . "$env:USERPROFILE/AppData/Roaming/EmuDeck/backend/functions/allCloud.ps1"
