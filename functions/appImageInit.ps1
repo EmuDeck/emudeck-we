3c5cb090a9f06fd5488222bd98da45d8fc70b06c
Dragoon Dorise <rodrigosedano@me.com>
2025-02-24 15:33:25 +0100

    testing junction migration

--- functions/appImageInit.ps1
+++ functions/appImageInit.ps1
@@ -2,117 +2,46 @@ function appImageInit(){
 
 	SRM_resetLaunchers
 
-	#emudeck folder relocation
-	$path = "$env:USERPROFILE/EmuDeck"
-	if (Test-Path -Path $path -PathType Container) {
+	#Emulators new Path Junction
+	$carpetaA = "$env:USERPROFILE/EmuDeck/Emulators"
+	$carpetaB = "$env:APPDATA/EmuDeck/Emulators"
 
-	   confirmDialog -TitleText "Migration" -MessageText "We are going to move the $env:USERPROFILE/EmuDeck folder to $env:APPDATA\EmuDeck.Please wait until a new message confirms the migration"
+	# Verificar si la carpeta A es un directorio real
+	$esDirectorioA = (Test-Path $carpetaA -PathType Container)
 
-		$destination = "$env:APPDATA/EmuDeck"
+	# Verificar si la carpeta B no existe o si existe pero no es un Junction
+	$existeB = Test-Path $carpetaB -PathType Container
+	$esJunctionB = $false
 
-		$originalFolderName = Split-Path $path -Leaf
-		$newFolderName = Split-Path $destination -Leaf
-
-		Get-ChildItem -Path "$path" | ForEach-Object {
-			$destinationPath = Join-Path -Path $destination -ChildPath $_.Name
-			if (-Not (Test-Path -Path $destinationPath)) {
-			   Write-Host "Moving: $_.FullName to $destination" -ForegroundColor Blue
-			   Move-Item -Path $_.FullName -Destination $destination
-			} else {
-				Write-Host "El elemento ya existe en el destino: $destinationPath" -ForegroundColor Yellow
-			}
-		}
-
-		#createSaveLink $path "$env:APPDATA\EmuDeck\"
-
-		 . "$env:APPDATA/EmuDeck/settings.ps1"
-		 . "$env:APPDATA/EmuDeck/backend/vars.ps1"
-
-
-		 SRM_createParsers
-		 ESDE_init
-
-		 Remove-Item -Path $path -Recurse -Force
-
-		 $folders = Get-ChildItem -Path ("$steamInstallPath\userdata") -Director
-
-		   foreach ($folder in $folders) {
-
-			   $filePath = "$steamInstallPath\userdata\$folder\config\shortcuts.vdf"
-			   if (Test-Path -Path "$filePath") {
-
-				   $shorcutsPath = "$filePath"
-			   }
-		   }
-		  Copy-Item "$shorcutsPath" -Destination "$shorcutsPath.baknew" -ErrorAction SilentlyContinue
-		  $filePath = "$shorcutsPath"
-		  $content = Get-Content -Raw -Encoding Default $filePath
-		  $newContent = $content -replace [regex]::Escape("EmuDeck\Emulators"), "AppData\Roaming\EmuDeck\Emulators"
-		  $newContent = $newContent -replace [regex]::Escape("EmuDeck\EmulationStation-DE\Emulators"), "AppData\Roaming\EmuDeck\Emulators"
-		  Set-Content -Path $filePath -Value $newContent -Encoding Default
-
-		 confirmDialog -TitleText "Complete" -MessageText "Migration complete,you can now use EmuDeck as always. The Emulation folder is still at $emulationPath"
-	 }
-
-	# Copy-Item "$shorcutsPath" -Destination "$shorcutsPath.bak" -ErrorAction SilentlyContinue
-
-
-	$folders = Get-ChildItem -Path ("$steamInstallPath\userdata") -Director
-
-	   foreach ($folder in $folders) {
-
-		   $filePath = "$steamInstallPath\userdata\$folder\config\shortcuts.vdf.bak"
-		   if (Test-Path -Path "$filePath") {
-			  $shorcutsPath = "$filePath"
-			  $result = yesNoDialog -TitleText "SRM Backup" -MessageText "Is your Steam Library corrupted? Do you want to restore your last backup?" -OKButtonText "Yes" -CancelButtonText "No"
-
-			  if ($result -eq "OKButton") {
-				 $steamRunning = Get-Process -Name "Steam" -ErrorAction SilentlyContinue
-				 if ($steamRunning) {
-					 taskkill /IM steam.exe /F
-				 }
-				 Rename-Item -Path "$steamInstallPath\userdata\$folder\config\shortcuts.vdf" -NewName "$steamInstallPath\userdata\$folder\config\shortcuts.vdf.bak2"
-				 Copy-Item "$shorcutsPath" -Destination "$steamInstallPath\userdata\$folder\config\shortcuts.vdf" -ErrorAction SilentlyContinue
-				 Rename-Item -Path "$shorcutsPath" -NewName "$shorcutsPath.restored"
-
-
-			  }else{
-				 Rename-Item -Path "$shorcutsPath" -NewName "$shorcutsPath.cancel"
-			  }
-
-		   }
-	   }
-
-	 $folders = Get-ChildItem -Path ("$steamInstallPath\userdata") -Director
-
-		foreach ($folder in $folders) {
-
-			$filePath = "$steamInstallPath\userdata\$folder\config\shortcuts.vdf"
-			if (Test-Path -Path "$filePath") {
-
-				$shorcutsPath = "$filePath"
-			}
-		}
-	   Copy-Item "$shorcutsPath" -Destination "$shorcutsPath.baknew" -ErrorAction SilentlyContinue
-	   $filePath = "$shorcutsPath"
-	   $content = Get-Content -Raw -Encoding Default $filePath
-	   $newContent = $content -replace [regex]::Escape("EmuDeck\Emulators"), "AppData\Roaming\EmuDeck\Emulators"
-	   $newContent = $newContent -replace [regex]::Escape("EmuDeck\EmulationStation-DE\Emulators"), "AppData\Roaming\EmuDeck\Emulators"
-	   $newContent = $newContent -replace [regex]::Escape("AppData\Roaming\AppData\Roaming"), "AppData\Roaming"
-	   Set-Content -Path $filePath -Value $newContent -Encoding Default
-
-	 $path = "$esdePath/Emulators"
-	 if (Test-Path -Path $path -PathType Container) {
-	  $item = Get-Item $path
-	  if ($item.Attributes -band [System.IO.FileAttributes]::ReparsePoint) {
-		Write-Output "$path it's a junction."
-	  } else {
-		Write-Output "$path it's a directory."
-		Move-Item -Path $path -Destination $emusPath
-		createSaveLink $path $emusPath
-		SRM_resetLaunchers
-	  }
+	if ($existeB) {
+		$esJunctionB = (Get-Item $carpetaB).Attributes -match "ReparsePoint"
 	}
+
+	# Si la carpeta A es un directorio y la carpeta B no existe o no es un junction, ejecutar pepito
+	if ($esDirectorioA -and (-not $existeB -or -not $esJunctionB)) {
+		createSaveLink $carpetaA $carpetaB
+	}
+
+	#ESDE new Path Junction
+	$carpetaA = "$env:USERPROFILE/EmuDeck/EmulationStation-DE"
+	$carpetaB = "$env:APPDATA/EmuDeck/EmulationStation-DE"
+
+	# Verificar si la carpeta A es un directorio real
+	$esDirectorioA = (Test-Path $carpetaA -PathType Container)
+
+	# Verificar si la carpeta B no existe o si existe pero no es un Junction
+	$existeB = Test-Path $carpetaB -PathType Container
+	$esJunctionB = $false
+
+	if ($existeB) {
+		$esJunctionB = (Get-Item $carpetaB).Attributes -match "ReparsePoint"
+	}
+
+	# Si la carpeta A es un directorio y la carpeta B no existe o no es un junction, ejecutar pepito
+	if ($esDirectorioA -and (-not $existeB -or -not $esJunctionB)) {
+		createSaveLink $carpetaA $carpetaB
+	}
+
 
 	#AutoFixes
 	mkdir "$emudeckFolder/feeds" -ErrorAction SilentlyContinue
