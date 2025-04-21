03339ee22fb41fd268b0c7784229f8841714ba6c
Matako <82229906+matakko@users.noreply.github.com>
2025-04-21 11:53:34 +0700

    Update appImageInit.ps1
    FIX \ and n3ds for switch

--- functions/appImageInit.ps1
+++ functions/appImageInit.ps1
@@ -134,7 +134,7 @@ function appImageInit(){
 	if (Select-String -Path $xmlPath -Pattern "Citra") {
 		confirmDialog -TitleText "Azahar ESDE fixed" -MessageText "There was an issue launching Azahar from ESDE, we have just automatically fixed it. Now you can play 3DS Games using Azahar from ESDE"
 		ESDE_init
-		Copy-Item "$env:APPDATA\EmuDeck\backend\configs\emulationstation\gamelists\n3ds\gamelist.xml" -Destination "$esdePath/ES-DE/gamelists/n3ds" -ErrorAction SilentlyContinue -Force
+		Copy-Item "$env:APPDATA\EmuDeck\backend\configs\emulationstation\gamelists\n3ds\gamelist.xml" -Destination "$esdePath\ES-DE\gamelists\n3ds\" -ErrorAction SilentlyContinue -Force
 	}
 
 	#Citron ESDE fix
@@ -146,13 +146,13 @@ function appImageInit(){
 		}else{
 			confirmDialog -TitleText "Citron ESDE fixed" -MessageText "There was an issue launching Citron from ESDE, we have just automatically fixed it. Now you can play your games using Citron from ESDE"
 			ESDE_init
-			Copy-Item "$env:APPDATA\EmuDeck\backend\configs\emulationstation\gamelists\switch\gamelist.xml" -Destination "$esdePath/ES-DE/gamelists/switch" -ErrorAction SilentlyContinue -Force
+			Copy-Item "$env:APPDATA\EmuDeck\backend\configs\emulationstation\gamelists\switch\gamelist.xml" -Destination "$esdePath\ES-DE\gamelists\switch\" -ErrorAction SilentlyContinue -Force
 		}
 	}else{
 		mkdir "$esdePath/ES-DE/gamelists/switch" -ErrorAction SilentlyContinue
 		confirmDialog -TitleText "Citron ESDE fixed" -MessageText "There was an issue launching Citron from ESDE, we have just automatically fixed it. Now you can play your games using Citron from ESDE"
 		ESDE_init
-		Copy-Item "$env:APPDATA\EmuDeck\backend\configs\emulationstation\gamelists\switch\gamelist.xml" -Destination "$esdePath/ES-DE/gamelists/switch" -ErrorAction SilentlyContinue -Force
+		Copy-Item "$env:APPDATA\EmuDeck\backend\configs\emulationstation\gamelists\switch\gamelist.xml" -Destination "$esdePath\ES-DE\gamelists\switch\" -ErrorAction SilentlyContinue -Force
 	}
 
 
