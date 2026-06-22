42405463c2b43b323dd8e63869c29eea2f5ceaba
José David Prieto Suárez <jdavidps95@gmail.com>
2026-06-22 15:47:41 +0200

    Fix parsers launcher srm (#133)

--- tools/launchers/srm/steamrommanager.ps1
+++ tools/launchers/srm/steamrommanager.ps1
@@ -1,9 +1,21 @@
 . "$($env:USERPROFILE)\AppData\Roaming\EmuDeck\backend\functions\all.ps1"
+
+function SRM_checkParsers {
+    $userConfig = "$toolsPath\userData\userConfigurations.json"
+    $parsers = Get-Content $userConfig -Raw -ErrorAction SilentlyContinue | ConvertFrom-Json -ErrorAction SilentlyContinue
+
+    if (!$parsers -or $parsers.Count -eq 0) {
+        Write-Host "Steam ROM Manager has no parsers, restoring configuration..."
+        SRM_createParsers
+    }
+}
+
 $steamRunning = Get-Process -Name "Steam" -ErrorAction SilentlyContinue
 if ($steamRunning) {
     taskkill /IM steam.exe /F
 }
 RPCS3_renameFolders
+SRM_checkParsers
 Write-Host "Launching Steam Rom Manager, please stand by..." -ForegroundColor white
 $emulatorFile = "$toolsPath/srm.exe"
 $scriptFileName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
