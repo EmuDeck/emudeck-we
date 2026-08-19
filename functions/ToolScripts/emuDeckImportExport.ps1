6ce806300589c8f80694ddc80fb64590e7ac2075
Rodrigo <rodrigosedano@me.com>
2026-08-19 17:58:12 +0200

    fix

--- functions/ToolScripts/emuDeckImportExport.ps1
+++ functions/ToolScripts/emuDeckImportExport.ps1
@@ -2,6 +2,7 @@ function import_emudeck($items, $origin) {
 	generate_pythonEnv | Out-Null
 	$env:emulationPath = $emulationPath
 	$env:ESDEscrapData = $ESDEscrapData
+	$items = $items.Replace('"', '\"')
 	python "$emudeckBackend\tools\importExport.py" import_emudeck "$items" "$origin"
 	return $LASTEXITCODE
 }
@@ -10,6 +11,7 @@ function export_emudeck($items, $destination) {
 	generate_pythonEnv | Out-Null
 	$env:emulationPath = $emulationPath
 	$env:ESDEscrapData = $ESDEscrapData
+	$items = $items.Replace('"', '\"')
 	python "$emudeckBackend\tools\importExport.py" export_emudeck "$items" "$destination"
 	return $LASTEXITCODE
 }
