a5137610fcda89e35606b653b01f4098b062bcda
Rodrigo <rodrigosedano@me.com>
2026-08-31 11:45:50 +0200

    proper name

--- functions/EmuScripts/emuDeckPCSX2QT.ps1
+++ functions/EmuScripts/emuDeckPCSX2QT.ps1
@@ -6,7 +6,7 @@ function PCSX2QT_install(){
 	setMSG "Downloading PCSX2"
 	$url_pcsx2 = getReleaseURLGH "pcsx2/pcsx2" "7z" "windows" "symbols"
 	download $url_pcsx2 "pcsx2.7z"
-	Rename-Item -Path "$temp\pcsx2" -NewName "pcsx2-qtx64.exe" -ErrorAction SilentlyContinue
+	Rename-Item -Path "$temp\pcsx2\pcsx2-qt.exe" -NewName "pcsx2-qtx64.exe" -ErrorAction SilentlyContinue
 	moveFromTo "$temp\pcsx2" "$emusPath\PCSX2-Qt"
 	Remove-Item -Recurse -Force $temp\pcsx2 -ErrorAction SilentlyContinue	
 	createLauncher "pcsx2"
